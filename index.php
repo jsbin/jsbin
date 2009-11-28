cc8247e1cef48b4269efd05aa429b47267d6c07f
Remy Sharp <remy@remysharp.com>
2009-11-28 15:13:37 +0000

    Added code & preview buttons with styling, and created new render preview method (also supports just JavaScript)

--- index.php
+++ index.php
@@ -3,183 +3,14 @@
 <head>
 <meta charset=utf-8 />
 <title>JS Bin - Collaborative JavaScript Debugging</title>
-<style>
-body { 
-  margin: 0; 
-  padding: 0;
-  font-family: "Helvetica Neue";
-  font-size: 13px;
-  min-width: 976px;
-  overflow: hidden;
-  background: url(/images/jsbin-bg.png) repeat-x 0 -3px;
-}
-
-p {
-  margin: 0;
-}
-
-#control {
-  height: 63px;
-  position: relative;
-  width: 100%;
-}
-
-.control, .help, .starting {
-  width: 100px;
-  padding: 15px;
-  float: left;
-}
-
-.control, .starting {
-  width: 30%;
-}
-
-.starting {
-  text-align: center;
-}
-
-.help {
-  width: 300px;
-  text-align: right;
-  float: right;
-}
-
-ul.flat {
-  margin: 0;
-  padding: 0;
-}
-
-ul.flat li {
-  float: left;
-  display: block;
-  list-style: none;
-  margin: 0 0 0 15px;
-  padding: 0;
-}
-
-.help ul.flat {
-  float: right;
-}
-
-#control > *, a {
-  color: #232323;
-  text-shadow: #fff 1px 1px 1px;
-}
-
-a {
-  font-weight: bold;
-}
-
-a:hover {
-  text-shadow: #fff -1px -1px 3px;
-}
-
-.light {
-  font-weight: normal;
-  text-decoration: none;
-}
-
-.stretch {
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-}
-
-#bin {
-  top: 64px;
-  width: 100%;
-}
-
-div.html {
-  left: 50%;
-}
-
-/*.mozilla #bin div.html {
-  margin-left: -2px;
-}
-
-.mozilla #bin.javascript div.javascript,
-.mozilla #bin div.html {
-  border: 1px solid #0080FF;
-}
-
-#bin.javascript div.html,
-#bin div.javascript {
-  border: 0;
-}
-*/
-div.code {
-  width: 50%;
-}
-
-div.preview {
-  display: none;
-  width: 100%;
-}
-
-.code p {
-  font-weight: bold;
-  background: #fff;
-  padding: 10px;
-  padding-right: 0;
-  margin-left: 1px;
-}
-
-iframe.javascript {
-  border-right: 1px solid #ccc !important;
-}
-
-.buttons {
-  float: left;
-  display: block;
-  margin-right: 10px;
-}
-
-body.preview #source,
-body.source #preview {
-  display: none;
-}
-
-body.preview #preview,
-body.source #source {
-  display: block;
-}
-
-a.button {
-  border: 1px solid #ccc;
-  -webkit-border-radius: 3px;
-  -moz-border-radius: 3px;
-  border-radius: 3px;
-  height: 12px;
-  line-height: 12px;
-  padding: 10px;
-  display: block;
-  float: left;
-  text-decoration: none;
-  margin: 0px 5px;
-  background: #fff;
-  background: rgba(255, 255, 255, 0.3);
-}
-
-body.source a.source,
-body.preview a.preview {
-  background: #000;
-  background: rgba(0, 0, 0, 0.25);
-  text-shadow: none;
-  border: 1px solid #ccc;
-  -moz-box-shadow: #fff 0px 0px 5px;
-  -webkit-box-shadow: #fff 0px 0px 5px;
-}
-</style>
+<link rel="stylesheet" href="/css/style.css" type="text/css" />
 </head>
 <body class="source">
 <div id="control">
   <div class="control">
     <div class="buttons">
-      <a class="button source" href="#source">Code</a>
-      <a class="button preview" href="#preview">Preview</a>
+      <a class="button source" accesskey="1" href="#source">Code</a>
+      <a class="button preview" accesskey="2" href="#preview">Preview</a>
     </div>
     <a href="http://jsbin.com/ebabu">http://jsbin.com/ebabu</a>
     <p><a class="light" id="revert" href="#">Revert</a> &mdash; <a class="light" href="#">New milestone</a></p>
@@ -205,7 +36,7 @@ body.preview a.preview {
     </div>
   </div>
   <div id="preview" class="binview stretch">
-    
+    <iframe class="stretch"></iframe>
   </div>
 </div>
 <script src="js/vendor/codemirror/codemirror.js" type="text/javascript"></script>
--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+(function () {
+  
 $('#startingpoint').click(function () {
   if (localStorage) {
     localStorage.setItem('saved-javascript', editors.javascript.getCode());
@@ -9,26 +11,72 @@ $('#startingpoint').click(function () {
 $('#revert').click(function () {
   sessionStorage.removeItem('javascript');
   sessionStorage.removeItem('html');
-  
+
   populateEditor('javascript');
   populateEditor('html');
-  
+
   editors.javascript.focus();
-  
+
   return false;
 });
 
-$('#control .button').click(function () {
+$('#control .button').click(function (event) {
+  event.preventDefault();
   $('body').removeAttr('class').addClass(this.hash.substr(1));
+
+  if ($(this).is('.preview')) {
+    renderPreview();
+  } 
 });
+
+// $(document).bind('online', function () {
+//   console.log("we're online");
+// }).bind('offline', function () {
+//   console.log("we're offline");
+// });
 
 // has to be first because we need to set first
 $(window).unload(function () {
   sessionStorage.setItem('javascript', editors.javascript.getCode());
   sessionStorage.setItem('html', editors.html.getCode());
-  
+
   var panel = getFocusedPanel();
   sessionStorage.setItem('panel', panel);
   sessionStorage.setItem('line', editors[panel].currentLine());
   sessionStorage.setItem('character', editors[panel].cursorPosition().character);
 });
+
+function renderPreview() {
+  var doc = $('#preview iframe')[0], 
+      win = doc.contentDocument || doc.contentWindow.document,
+      source = editors.html.getCode();
+  
+  if (!$.trim(source)) {
+    source = "<pre>\n" + editors.javascript.getCode() + "</pre>";
+  } else if (/%code%/.test(source)) {
+    source = source.replace(/%code%/, editors.javascript.getCode());
+  } else { // insert before the body close tag
+    source = source.replace(/<\/body>/, "<script>\n" + editors.javascript.getCode() + "\n</script>\n</body>");
+  }
+  
+  win.open();
+  win.write(source);
+  win.close();
+}  
+
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

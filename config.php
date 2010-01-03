8503d127786633e726d52b3eddfea4fe35262c23
Remy Sharp <remy@remysharp.com>
2010-01-03 00:59:21 +0000

    Sample database connection config

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+// database settings
+define('DB_NAME', 'jsbin');
+define('DB_USER', 'jsbin_user');  // Your MySQL username
+define('DB_PASSWORD', 'jsbin_password'); // ...and password
+define('DB_HOST', 'localhost');  // 99% chance you won't need to change this value
+?>

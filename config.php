33ba970914329aa2c8ca333d84acf4258175151f
Remy Sharp <remy@remysharp.com>
2010-01-05 00:52:38 +0000

    config designed to offline and online versions of code base

--- config.php
+++ config.php
@@ -4,4 +4,14 @@ define('DB_NAME', 'jsbin');
 define('DB_USER', 'jsbin_user');  // Your MySQL username
 define('DB_PASSWORD', 'jsbin_password'); // ...and password
 define('DB_HOST', 'localhost');  // 99% chance you won't need to change this value
+
+// change this to suite your offline detection
+define('OFFLINE', is_dir('/Users/'));
+
+define('MAJOR', 2);
+define('MINOR', 0);
+define('TINY', 0);
+
+// wishing PHP were more like JavaScript...wishing I was able to use Node.js they way I had wanted...
+define('VERSION', OFFLINE ? 'debug' : join(array(MAJOR, MINOR, TINY), '.'));
 ?>

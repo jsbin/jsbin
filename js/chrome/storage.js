0d3a23a797d93d9d71d55f5917055970f30d711d
Remy Sharp <remy@leftlogic.com>
2010-01-21 17:20:27 +0000

    Rewrite of the storage logic to get around Firefox requiring cookies enabled to even access the global object. Fixes issue #5 f:30

--- js/chrome/storage.js
+++ js/chrome/storage.js
@@ -1,8 +1,30 @@
 // I would like to lazy load json2, but I'm worried we're trying to access
 // JSON very early in the code
 //= require <json2>
-if (!window.sessionStorage) {
-  window.sessionStorage = (function () {
+
+var requiresCookies = (function () {
+  var ua = navigator.userAgent.toLowerCase(),
+      id = null;
+  if (/mozilla/.test(ua) && !/compatible/.test(ua)) {
+    id = new Date().getTime();
+    document.cookie = '__cprobe=' + id + ';path=/';
+    if (document.cookie.indexOf(id,0) === -1) {
+      return true;
+    }
+  } 
+  return false;
+})();
+
+// Firefox with Cookies disabled triggers a security error when we probe window.sessionStorage
+// currently we're just disabling all the session features if that's the case.
+var sessionStorage, localStorage;
+
+if (!requiresCookies && window.sessionStorage) {
+  sessionStorage = window.sessionStorage;
+}
+
+if (!sessionStorage) {
+  sessionStorage = (function () {
     var data = window.top.name ? JSON.parse(window.top.name) : {};
 
     return {
@@ -25,7 +47,9 @@ if (!window.sessionStorage) {
   })();
 }
 
-if (!window.localStorage) {
+if ((!requiresCookies && !window.localStorage) || requiresCookies) {
   // dirty, but will do for our purposes
-  window.localStorage = window.sessionStorage;
+  localStorage = sessionStorage;
+} else if (!requiresCookies) {
+  localStorage = window.localStorage;
 }

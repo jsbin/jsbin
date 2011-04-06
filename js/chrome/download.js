2d1b7722cb49a3dc0bbfb1e48a1d26caba3e6d97
remy <remy@remysharp.com>
2011-04-06 15:38:26 +0100

    Fixed the version jumping in download, and made save use ajax to save and history pushState for versioning

--- js/chrome/download.js
+++ js/chrome/download.js
@@ -4,42 +4,15 @@ function enableDownload() {
   var $revert = $('#revert');
   $('#download').click(function (event) {
     event.preventDefault();
-    var url = window.location.pathname,
-        parts = [];
 
     // saveCode is via save.js
     if ($revert.is(':not(.enable)') || $revert.is(':hidden') || url.indexOf('/edit') === -1) {
       // not edited, so don't change the version and save
       saveCode('download');
-      return;
     } else {
-      saveCode('download,save');
-    }
-
-    parts = url.split('/');
-    parts.pop(); // "edit"
-    var revision = parts.pop();
-    var code_id = parts.pop();
-    if (!/\d+/.test(revision)) {
-      code_id = revision;
-      revision = 1;
-    }
-
-    revision++;
-
-    url = '/' + code_id + '/' + revision;
-
-    if (window.history && window.history.pushState) {
-      window.history.pushState(null, url + '/edit', url + '/edit');
-
-      $('form').attr('action', url + '/save');
-      var link = $('#jsbinurl').attr('href', url)[0];
-
-      if (link.href) {
-        $('#jsbinurl').text(link.href);
-      }
-    } else {
-      window.location = url + '/edit';
+      saveCode('save', true, function () {
+        saveCode('download');
+      }); // triggers via ajax
     }
   });
 }
--- js/chrome/save.js
+++ js/chrome/save.js
@@ -1,12 +1,12 @@
 // to allow for download button to be introduced via beta feature
 $('#save').click(function (event) {
   event.preventDefault();
-  saveCode('save');
+  saveCode('save', window.location.pathname.indexOf('/edit') !== -1);
   
   return false;
 });
 
-function saveCode(method) {
+function saveCode(method, ajax, ajaxCallback) {
   // create form and post to it
   var $form = $('form')
     .append('<input type="hidden" name="javascript" />')
@@ -15,6 +15,29 @@ function saveCode(method) {
   $form.find('input[name=javascript]').val(editors.javascript.getCode());
   $form.find('input[name=html]').val(editors.html.getCode());
   $form.find('input[name=method]').val(method);
-  
-  $form.submit();
+  if (ajax) {
+    $.ajax({
+      url: $form.attr('action'),
+      data: $form.serialize(),
+      dataType: 'json', 
+      type: 'post',
+      success: function (data) {
+        $('form').attr('action', data.url + '/save');
+        ajaxCallback && ajaxCallback();
+
+        if (window.history && window.history.pushState) {
+          window.history.pushState(null, data.edit, data.edit);
+
+          $('#jsbinurl').attr('href', data.edit).text(data.edit);
+        } else {
+          window.location = data.edit;
+        }
+      },
+      error: function () {
+
+      }
+    });
+  } else {
+    $form.submit();
+  }
 }

9fd0670f9f1d40d07fee902a465fb75f73d46eac
remy <remy@remysharp.com>
2011-06-24 00:53:12 +0100

    Switched to using utf8 in database (why-o-why was it latin1 in the first place!)

--- build/jsbin.sql
+++ build/jsbin.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS `sandbox` (
   `id` int(11) NOT NULL auto_increment,
-  `javascript` text,
-  `html` text,
+  `javascript` text character set utf8,
+  `html` text character set utf8,
   `created` datetime default NULL,
   `last_viewed` datetime default NULL,
   `url` char(255) default NULL,
@@ -19,4 +19,14 @@ CREATE TABLE IF NOT EXISTS `sandbox` (
   KEY `streaming_key` (`streaming_key`),
   KEY `spam` (`created`,`last_viewed`),
   KEY `revision` (`url`, `revision`)
+) character set utf8;
+
+CREATE TABLE IF NOT EXISTS `owners` (
+  `id` int(11) NOT NULL auto_increment,
+  `name` char(255) NOT NULL,
+  `url` char(255) NOT NULL,
+  `revision` int(11) default 1,
+  
+  PRIMARY KEY  (`id`),
+  KEY `name_url` (`name`, `url`, `revision`)
 );

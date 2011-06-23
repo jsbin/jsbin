a586b88031b94279fd437c49c67b0622c58e5264
remy <remy@remysharp.com>
2011-06-24 00:54:58 +0100

    Coming support for personalisation of bins saved.

--- js/chrome/beta.js
+++ js/chrome/beta.js
@@ -15,5 +15,18 @@
   this.active = localStorage.getItem('beta') == 'true' || false;
   if (this.active) this.on();
   
+  this.home = function (name) {
+    jsbin.settings.home = name; // will save later
+    
+    // cookie is required to share with the server so we can do a redirect on new bin
+    var date = new Date();
+		date.setTime(date.getTime()+(365*24*60*60*1000)); // set for a year
+    document.cookie = 'home=' + name + '; expires=' + date.toGMTString() + ' path=/';
+    
+    if (location.pathname == '/') {
+      location.reload();
+    }
+  };
+  
   //= require "stream"  
 }).call(jsbin);

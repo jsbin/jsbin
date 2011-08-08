60311912a3899be0d2d8dd016f7a2c699270beb9
remy <remy@remysharp.com>
2011-08-08 09:21:17 +0100

    Container for remote live rendering.

---
+++ live.php
@@ -0,0 +1,16 @@
+<?php
+include('config.php'); 
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset=utf-8>
+<title>Real-time live JS Bin</title>
+<style>
+body { margin: 0; padding: 0; overflow: hidden }
+</style>
+</head>
+<body>
+<script src="<?php echo ROOT ?>js/render/remote.js"></script>
+</body>
+</html>

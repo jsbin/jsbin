9fcde3e27fb7c34509fa9a3ea4c2b2be8aa9b57d
Remy Sharp <remy@remysharp.com>
2011-11-03 20:45:39 +0000

    +jQuery 1.7

--- js/editors/libraries.js
+++ js/editors/libraries.js
@@ -44,8 +44,8 @@ Libraries.prototype.init = function () {
       scripts: [
         { text: 'jQuery latest', url: 'http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js' },
         { text: 'jQuery WIP (via git)', url: 'http://code.jquery.com/jquery-git.js' },
-        { text: 'jQuery 1.6.4', url: 'http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js' },
-        { text: 'jQuery 1.5.2', url: 'http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js' }
+        { text: 'jQuery 1.7.0', url: 'http://code.jquery.com/jquery-1.7.min.js' },
+        { text: 'jQuery 1.6.4', url: 'http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js' }
       ]
     },
     jqueryui : {

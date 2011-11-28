a05b4f2df5672c9ed9976440d3f3506c5fc61a4a
Jason Crane <jasonlcrane@gmail.com>
2011-11-28 16:12:21 +0000

    Updated jquerymobile style to pull correct css file

--- js/editors/libraries.js
+++ js/editors/libraries.js
@@ -59,7 +59,7 @@ Libraries.prototype.init = function () {
     jquerymobile : {
       text: 'jQuery Mobile',
       requires: 'http://code.jquery.com/jquery-1.6.4.min.js',
-      style: 'http://code.jquery.com/mobile/1.0b3/jquery.mobile-1.0.min.css',
+      style: 'http://code.jquery.com/mobile/1.0/jquery.mobile-1.0.min.css',
       scripts: [
         { text: 'jQuery Mobile 1.0', url: 'http://code.jquery.com/mobile/1.0/jquery.mobile-1.0.min.js' },
         { text: 'jQuery Mobile 1.0b3', url: 'http://code.jquery.com/mobile/1.0b3/jquery.mobile-1.0b3.min.js' }

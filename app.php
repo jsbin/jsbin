b4a687473b12c22dfd1e74a4396650d21fe7190d
Remy Sharp <remy@remysharp.com>
2012-04-07 22:41:23 +0100

    Fixed latest shortcut

--- app.php
+++ app.php
@@ -273,7 +273,7 @@ if (!$action) {
     // find the latest revision and redirect to that.
     $code_id = $subaction;
     $latest_revision = getMaxRevision($code_id);
-    // header('Location: /' . $code_id . '/' . $latest_revision);
+    header('Location: /' . $code_id . '/' . $latest_revision);
     $edit_mode = false;
   }
   // gist are formed as jsbin.com/gist/1234 - which land on this condition, so we need to jump out, just in case

224fcd129646f21a215c8967145cd0923d16a9bb
Remy Sharp <remy@remysharp.com>
2012-04-10 15:54:25 +0100

    Trying to bring revert back to life (not quite working yet)

--- js/editors/unsaved.js
+++ js/editors/unsaved.js
@@ -1,25 +1,28 @@
-var $revert = $('#revert'); //.next().addClass('left').end();
-$(document).bind('codeChange', function (event, revert, onload) {
-  if (revert == undefined) {
+var $revert = $('#revert'); 
+
+$(document).bind('codeChange', function (event, options) { //, revert, onload) {
+  options || (options = {});
+
+  if (options.revert == undefined) {
     revert = false;
   } else {
     $revert.removeClass('enable');
+    revert = options.revert;
   }
-  
-  updateTitle(revert, onload);
+
+  updateTitle(revert, options.onload);
 });
 
 function updateTitle(revert, onload) {
+  console.log('revert required? ' + revert);
   var title = !documentTitle ? 'JS Bin' : documentTitle;
   if (jsbin.settings.home) title = jsbin.settings.home + '@' + title;
-  if (jsbin.panels.ready) {
-    if (!revert) {
-      document.title = title + ' [unsaved]';
-      if ($revert.addClass('enable').is(':hidden')) {
-        $revert[onload ? 'show' : 'fadeIn']().next().removeClass('left');
-      }
-    } else {
-      document.title = title;
-    }    
+  if (!revert) {
+    document.title = title + ' [unsaved]';
+    if ($revert.addClass('enable').is(':hidden')) {
+      $revert[onload ? 'show' : 'fadeIn']().next().removeClass('left');
+    }
+  } else {
+    document.title = title;
   }
 }

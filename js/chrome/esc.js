e37d98e5ee06a877286eef4001b1a099cf33ea5d
Remy Sharp <remy@remysharp.com>
2012-04-14 10:56:17 +0100

    Click outside of modal box to close. Fixes #143

--- js/chrome/esc.js
+++ js/chrome/esc.js
@@ -3,20 +3,30 @@ var loginVisible = false,
     keyboardHelpVisible = false;
 $document.keydown(function (event) {
   if (event.which == 27 || (keyboardHelpVisible && event.which == 191 && event.shiftKey && event.metaKey)) {
-    if (keyboardHelpVisible) {
-      $body.toggleClass('keyboardHelp');
-      keyboardHelpVisible = false;
-    } else if (prefsOpen) {
-      $body.toggleClass('prefsOpen');
-      prefsOpen = false;
-    } else if (dropdownOpen) {
-      $('.menu.open').removeClass('open');
-      dropdownOpen = false;
-    } else if (loginVisible) {
-      $('#login').hide();
-      loginVisible = false;
-    } else if ($('#history').length && !$body.hasClass('panelsVisible')) {
-      $('#history').toggle(100);
-    }
+    hideOpen();
+  }
+});
+
+function hideOpen() {
+  if (keyboardHelpVisible) {
+    $body.removeClass('keyboardHelp');
+    keyboardHelpVisible = false;
+  } else if (prefsOpen) {
+    $body.removeClass('prefsOpen');
+    prefsOpen = false;
+  } else if (dropdownOpen) {
+    $('.menu.open').removeClass('open');
+    dropdownOpen = false;
+  } else if (loginVisible) {
+    $('#login').hide();
+    loginVisible = false;
+  } else if ($('#history').length && !$body.hasClass('panelsVisible')) {
+    $('#history').toggle(100);
+  }
+}
+
+$document.delegate('.modal', 'click', function (event) {
+  if ($(event.target).is('.modal')) {
+    hideOpen();
   }
 });

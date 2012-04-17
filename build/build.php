ef05666727133fdc5cc231230dc0d22658df8fd5
Aron Carroll <self@aroncarroll.com>
2012-04-17 11:38:35 +0100

    Move PHP codebase into ./public directory
    Updated all relevant paths to point to new locations. Webs servers will
    need to be updated to point to ./public. The build script remains in the
    root and will work as before.

--- build/build.php
+++ build/build.php
@@ -2,20 +2,20 @@
 
 define('VERSION', trim(file_get_contents('VERSION')));
 
-define('SPROCKETIZED', './js/tmp.' . VERSION . '.js');
-define('PRODUCTION', './js/jsbin-' . VERSION . '.js');
+define('SPROCKETIZED', './public/js/tmp.' . VERSION . '.js');
+define('PRODUCTION', './public/js/jsbin-' . VERSION . '.js');
 
-// sprocketize the code in to _jsbin.VERSION.js
-require_once('lib/sprockets/sprocket.php');
+// sprocketize the code in to jsbin.VERSION.js
+require_once('vendor/sprockets/sprocket.php');
 
 echo "Sprocketizing...\n";
 
-$filePath = './js/jsbin.js';
+$filePath = './public/js/jsbin.js';
 // prepare sprocket
 $sprocket = new Sprocket($filePath, array(    
   'contentType' => '', // keeps debug quiet
-  'baseUri' => '../js',
-  'baseFolder' => array('./js/vendor', './js/vendor/codemirror2'),
+  'baseUri' => '../public/js',
+  'baseFolder' => array('./public/js/vendor', './public/js/vendor/codemirror2'),
   'assetFolder' => '..',
   'debugMode' => true, // forces to always show
   'autoRender' => false
@@ -31,7 +31,7 @@ file_put_contents(SPROCKETIZED, $js);
 
 // google compile in to jsbin.VERSION.js
 echo "Google compiler compressing...\n";
-system('java -jar "./lib/compiler.jar" --js="' . SPROCKETIZED . '" --js_output_file="' . PRODUCTION . '" --warning_level=QUIET');
+system('java -jar "./vendor/compiler.jar" --js="' . SPROCKETIZED . '" --js_output_file="' . PRODUCTION . '" --warning_level=QUIET');
 
 unlink(SPROCKETIZED);
 echo "Compressed: " . PRODUCTION . "\nFile size: " . filesize(PRODUCTION) . " bytes.\n";

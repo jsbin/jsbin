15deac7701eff4adc02171df47403060a0c3f9d4
Remy Sharp <remy@remysharp.com>
2012-05-04 10:16:11 +0100

    Got the iframe reset order wrong - it was injecting the JS in to the first - so growing in memory!

--- js/render/live.js
+++ js/render/live.js
@@ -42,7 +42,7 @@ function two(s) {
 function renderLivePreview(withalerts) {
   var source = getPreparedCode(),
       remove = $live.find('iframe').length > 0,
-      frame = $live.append('<iframe class="stretch" frameBorder="0"></iframe>').find('iframe:first')[0],
+      frame = $live.prepend('<iframe class="stretch" frameBorder="0"></iframe>').find('iframe:first')[0],
       doc = frame.contentDocument || frame.contentWindow.document,
       win = doc.defaultView || doc.parentWindow,
       d = new Date();
@@ -59,59 +59,68 @@ function renderLivePreview(withalerts) {
   var run = function () {
     var jsbinConsole = jsbin.panels.panels.console.visible ? 'window.top._console' : false;
 
-    doc.open();
+    // we're wrapping the whole thing in a try/catch in case the doc.write breaks the
+    // execution and never reaches the point where it removes the previous iframe.
+    try {
+      doc.open();
 
-    if (debug) {
-      doc.write('<pre>' + source.replace(/[<>&]/g, function (m) {
-        if (m == '<') return '&lt;';
-        if (m == '>') return '&gt;';
-        if (m == '"') return '&quot;';
-      }) + '</pre>');
-    } else {
-      // nullify the blocking functions
-      // IE requires that this is done in the script, rather than off the window object outside of the doc.write
-      if (withalerts !== true) {
-        doc.write(killAlerts);
+      if (debug) {
+        doc.write('<pre>' + source.replace(/[<>&]/g, function (m) {
+          if (m == '<') return '&lt;';
+          if (m == '>') return '&gt;';
+          if (m == '"') return '&quot;';
+        }) + '</pre>');
       } else {
+        // nullify the blocking functions
+        // IE requires that this is done in the script, rather than off the window object outside of the doc.write
+        if (withalerts !== true) {
+          doc.write(killAlerts);
+        } else {
+          doc.write(restoreAlerts);
+        }
+
+        if (jsbinConsole) {
+          doc.write('<script>(function(){window.addEventListener && window.addEventListener("error", function (event) { window.top._console.error({ message: event.message }, event.filename + ":" + event.lineno);}, false);}());</script>');
+
+          // doc.write('<script>(function () { var fakeConsole = ' + jsbinConsole + '; if (console != undefined) { for (var k in fakeConsole) { console[k] = fakeConsole[k]; } } else { console = fakeConsole; } })(); window.onerror = function () { console.error.apply(console, arguments); }</script>');
+        }
+
+        // almost jQuery Mobile specific - when the page renders
+        // it moves the focus over to the live preview - since 
+        // we no longer have a "render" panel, our code loses 
+        // focus which is damn annoying. So, I cancel the iframe
+        // focus event...because I can :)
+        var click = false;
+        win.onmousedown = function () {
+          click = true;
+          setTimeout(function () {
+            click = false;
+          }, 10);
+        };
+        win.onfocus = function (event) {
+          // allow the iframe to be clicked to create a fake focus
+          if (click) {
+            $('#live').focus();
+          }
+          // return false;
+        };
+
+        doc.write(source);
         doc.write(restoreAlerts);
       }
-
-      if (jsbinConsole) {
-        doc.write('<script>(function(){window.addEventListener && window.addEventListener("error", function (event) { window.top._console.error({ message: event.message }, event.filename + ":" + event.lineno);}, false);}());</script>');
-
-        // doc.write('<script>(function () { var fakeConsole = ' + jsbinConsole + '; if (console != undefined) { for (var k in fakeConsole) { console[k] = fakeConsole[k]; } } else { console = fakeConsole; } })(); window.onerror = function () { console.error.apply(console, arguments); }</script>');
-      }
-
-      // almost jQuery Mobile specific - when the page renders
-      // it moves the focus over to the live preview - since 
-      // we no longer have a "render" panel, our code loses 
-      // focus which is damn annoying. So, I cancel the iframe
-      // focus event...because I can :)
-      var click = false;
-      win.onmousedown = function () {
-        click = true;
-        setTimeout(function () {
-          click = false;
-        }, 10);
-      };
-      win.onfocus = function (event) {
-        // allow the iframe to be clicked to create a fake focus
-        if (click) {
-          $('#live').focus();
-        }
-        // return false;
-      };
-
-      doc.write(source);
-      doc.write(restoreAlerts);
+      doc.close();
+    } catch (e) {
+      console.error(e);
     }
-    doc.close();
+
     delete jsbin.panels.panels.live.doc;
     jsbin.panels.panels.live.doc = doc;
 
     // by removing the previous iframe /after/ the newly created live iframe
     // has run, it doesn't flicker - which fakes a smooth live update.
-    if (remove) $live.find('iframe:last').remove();
+    if (remove) {
+      $live.find('iframe:last').remove();
+    }
   }
 
   // WebKit requires a wait time before actually writing to the iframe
@@ -119,7 +128,7 @@ function renderLivePreview(withalerts) {
   if (iframedelay.active) {
     // this setTimeout allows the iframe to be rendered before our code
     // runs - thus allowing us access to the innerWidth, et al
-    setTimeout(run, 10);
+    setTimeout(run, 0);
   } else {
     run();
   }

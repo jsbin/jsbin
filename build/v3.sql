f5e5309e3012b6f458b5a1c0f3ee4fa4d4e624c9
Aron Carroll <self@aroncarroll.com>
2012-06-21 22:40:47 +0100

    Add SQL statements and table for the forgot_tokens table

--- build/v3.sql
+++ build/v3.sql
@@ -6,3 +6,12 @@ ALTER TABLE ownership
   ADD COLUMN last_login DATETIME NOT NULL,
   ADD COLUMN created DATETIME NOT NULL,
   ADD COLUMN updated DATETIME NOT NULL;
+CREATE TABLE forgot_tokens (
+  owner_name VARCHAR(255) NOT NULL,
+  token VARCHAR(255) NOT NULL,
+  expired INT(1) NOT NULL DEFAULT 0,
+  created DATETIME NOT NULL,
+  updated DATETIME NOT NULL,
+  KEY `index_created` (`created`),
+  KEY `index_token_expired_created` (`token`,`expired`,`created`)
+);

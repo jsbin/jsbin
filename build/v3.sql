f0624339e185430551df00de72efc3898730b455
Remy Sharp <remy@remysharp.com>
2012-06-27 16:20:15 +0100

    Pushing in @aron's changes from #190

--- build/v3.sql
+++ build/v3.sql
@@ -6,3 +6,11 @@ ALTER TABLE ownership
   ADD COLUMN last_login DATETIME NOT NULL,
   ADD COLUMN created DATETIME NOT NULL,
   ADD COLUMN updated DATETIME NOT NULL;
+CREATE TABLE forgot_tokens (
+  owner_name VARCHAR(255) NOT NULL,
+  token VARCHAR(255) NOT NULL,
+  expires DATETIME NOT NULL,
+  created DATETIME NOT NULL,
+  KEY `index_expires` (`expires`),
+  KEY `index_token_expires` (`token`,`expired`,`expires`)
+);

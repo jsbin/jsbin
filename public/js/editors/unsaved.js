0db66e6c86f5c0690f0dcc40257fe7220de2c1cd
Remy Sharp <remy@remysharp.com>
2012-07-13 09:50:12 +0200

    Removed title update stuff - should happen on render

--- public/js/editors/unsaved.js
+++ public/js/editors/unsaved.js
@@ -1,8 +1,8 @@
 // var $revert = $('#revert'); 
 
-$(document).bind('codeChange', function updateTitle(event, data) {
-  if (data.panelId === 'html') {
-    var title = !documentTitle ? 'JS Bin' : documentTitle;
-    document.title = title;
-  }
-});
+// $(document).bind('codeChange', function updateTitle(event, data) {
+//   if (data.panelId === 'html') {
+//     var title = !documentTitle ? 'JS Bin' : documentTitle;
+//     document.title = title;
+//   }
+// });

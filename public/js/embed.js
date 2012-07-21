b552ef1702b204ad7d5ea8297007657b44486e96
Remy Sharp <remy@remysharp.com>
2012-07-21 01:14:53 +0100

    Don't run twice, and do it when the DOM is ready.

--- public/js/embed.js
+++ public/js/embed.js
@@ -3,6 +3,65 @@
 // exit if we already have a script in place doing this task
 if (window.jsbinified !== undefined) return;
 
+/*!
+  * domready (c) Dustin Diaz 2012 - License MIT
+  */
+!function (name, definition) {
+  if (typeof module != 'undefined') module.exports = definition()
+  else if (typeof define == 'function' && typeof define.amd == 'object') define(definition)
+  else this[name] = definition()
+}('domready', function (ready) {
+
+  var fns = [], fn, f = false
+    , doc = document
+    , testEl = doc.documentElement
+    , hack = testEl.doScroll
+    , domContentLoaded = 'DOMContentLoaded'
+    , addEventListener = 'addEventListener'
+    , onreadystatechange = 'onreadystatechange'
+    , readyState = 'readyState'
+    , loaded = /^loade|c/.test(doc[readyState])
+
+  function flush(f) {
+    loaded = 1
+    while (f = fns.shift()) f()
+  }
+
+  doc[addEventListener] && doc[addEventListener](domContentLoaded, fn = function () {
+    doc.removeEventListener(domContentLoaded, fn, f)
+    flush()
+  }, f)
+
+
+  hack && doc.attachEvent(onreadystatechange, fn = function () {
+    if (/^c/.test(doc[readyState])) {
+      doc.detachEvent(onreadystatechange, fn)
+      flush()
+    }
+  })
+
+  return (ready = hack ?
+    function (fn) {
+      self != top ?
+        loaded ? fn() : fns.push(fn) :
+        function () {
+          try {
+            testEl.doScroll('left')
+          } catch (e) {
+            return setTimeout(function() { ready(fn) }, 50)
+          }
+          fn()
+        }()
+    } :
+    function (fn) {
+      loaded ? fn() : fns.push(fn)
+    })
+});
+
+// ---- here begins the jsbin embed - based on the embedding doc: https://github.com/remy/jsbin/blob/master/docs/embedding.md
+
+window.jsbinified = true;
+
 var innerText = document.createElement('i').innerText === undefined ? 'textContent' : 'innerText';
 
 // 1. find all links with class=jsbin
@@ -133,19 +192,21 @@ function embed(link) {
   }
 }
 
-// 2. process link based on subclass - jsbin-scoop to start with
-var links = getLinks(),
-    i = 0,
-    length = links.length,
-    className = '';
+domready(function () {
+  // 2. process link based on subclass - jsbin-scoop to start with
+  var links = getLinks(),
+      i = 0,
+      length = links.length,
+      className = '';
 
-for (; i < length; i++) {
-  className = ' ' + links[i].className + ' ';
-  if (className.indexOf(' jsbin-scoop ') !== -1) {
-    scoop(links[i]);
-  } else if (className.indexOf(' jsbin-embed ') !== -1) {
-    embed(links[i]);
+  for (; i < length; i++) {
+    className = ' ' + links[i].className + ' ';
+    if (className.indexOf(' jsbin-scoop ') !== -1) {
+      scoop(links[i]);
+    } else if (className.indexOf(' jsbin-embed ') !== -1) {
+      embed(links[i]);
+    }
   }
-}
+});
 
 }(this, document));

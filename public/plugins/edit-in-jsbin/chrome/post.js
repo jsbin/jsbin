460efeaafc061d06e62ef007fa5e65fff6bd08e6
Remy Sharp <remy@remysharp.com>
2012-08-10 14:29:46 +0100

    Updated 0.3

--- public/plugins/edit-in-jsbin/chrome/post.js
+++
@@ -1,7 +0,0 @@
-chrome.extension.onMessage.addListener(function(code) {
-  alert(JSON.stringify(code));
-  var input = document.getElementsByTagName('input')[0];
-  input.name = code.language;
-  input.value = encodeURIComponent(code.code);
-  input.form.submit();
-});

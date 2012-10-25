388c4cfc33d40989be448f144e75efbecbc0a89a
Scott González <scott.gonzalez@gmail.com>
2012-10-25 11:36:44 -0400

    Added jQuery UI 1.9.1.

--- public/js/editors/libraries.js
+++ public/js/editors/libraries.js
@@ -41,11 +41,11 @@ var libraries = [
     },
     {
         "url": [
-            "http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.0/themes/base/jquery-ui.css",
+            "http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/themes/base/jquery-ui.css",
             "http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js",
-            "http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.0/jquery-ui.min.js"
+            "http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js"
         ],
-        "label": "jQuery UI 1.9.0",
+        "label": "jQuery UI 1.9.1",
         "group": "jQuery UI"
     },
     {

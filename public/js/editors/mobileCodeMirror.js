596763993434353612ea1d294060566cd08fa969
Tom Ashworth <tom@phuu.net>
2012-11-26 12:09:03 +0000

    Save on blur on mobile (iOS), fix #410

--- public/js/editors/mobileCodeMirror.js
+++ public/js/editors/mobileCodeMirror.js
@@ -18,7 +18,7 @@ if (jsbin.mobile || jsbin.tablet || rootClassName.indexOf('ie6') !== -1 || rootC
     this.textarea.style.opacity = 1;
     // this.textarea.style.width = '100%';
 
-    $(this.textarea).keyup(throttle(function () {
+    $(this.textarea).blur(throttle(function () {
       $(document).trigger('codeChange', { panelId: el.id });
     }, 200));
 

0cc499a7d34ec1af6fc087380c0e6b17b396846e
Tom Ashworth <tom@phuu.net>
2013-01-09 16:50:33 +0000

    add support to the owners table for panels with content, and unified the setBinOwner, setBinForUser, assignBin, et al

--- build/populate.js
+++ build/populate.js
@@ -6,6 +6,12 @@ var options    = require('../lib/config'),
     handlers   = require('../lib/handlers'),
     utils      = require('../lib/utils');
 
+// DUMP
+//
+// ALTER TABLE owners ADD COLUMN html BOOLEAN NOT NULL DEFAULT 0, ADD COLUMN css BOOLEAN NOT NULL DEFAULT 0, ADD COLUMN javascript BOOLEAN NOT NULL DEFAULT 0;
+//
+// /DUMP
+
 // Get the full bin from an entry in the owners table
 
 var getBin = function (ownerBin, cb) {
@@ -27,17 +33,18 @@ var populateBin = function (ownerBin, done) {
     if (!sandboxBin) return done();
 
     ownerBin.summary = utils.titleForBin(sandboxBin);
+    ownerBin.html = !!sandboxBin.html;
+    ownerBin.css = !!sandboxBin.css;
+    ownerBin.javascript = !!sandboxBin.javascript;
 
     if (!ownerBin.last_updated || isNaN(ownerBin.last_updated.getTime())) { ownerBin.date = new Date(sandboxBin.created); }
     else { ownerBin.date = new Date(ownerBin.last_updated); }
 
 //    ownerBin.date = new Date(sandboxBin.created);
 
-    if (!ownerBin.summary) {
-      console.log('no summary for %d', ownerBin.id);
-    }
+    if (!ownerBin.summary) console.log('no summary for %d', ownerBin.id);
 
-    store.touchOwnership(ownerBin, done);
+    store.populateOwners(ownerBin, done);
   });
 };
 

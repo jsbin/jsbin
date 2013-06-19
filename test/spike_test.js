dda1b6bc63a74ec01cfdd7125c4d111e48491448
Tom Ashworth <tom@phuu.net>
2013-06-19 15:20:15 +0100

    Fix bug where falsey data was passed to makeEvent.

--- test/spike_test.js
+++ test/spike_test.js
@@ -1,6 +1,6 @@
 var spike = require('../lib/spike');
 
-describe('makeEvent', function () {
+describe('splike.utils.makeEvent', function () {
 
   it('should convert some string data into a valid event', function () {
     var result = spike.utils.makeEvent('example', 'hello');
@@ -21,6 +21,19 @@ describe('makeEvent', function () {
       'event:example',
       '\n'
     ].join('\n'));
+  });
+
+  it('should create an event even if no data is passed', function () {
+    var result = spike.utils.makeEvent('example');
+    result.should.equal([
+      'event:example',
+      '\n'
+    ].join('\n'));
+  });
+
+  it('should return nothing if nothing is passed', function () {
+    var result = spike.utils.makeEvent();
+    result.should.equal('');
   });
 
 });

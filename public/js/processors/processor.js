12f1876c98fa52c397b4eb97a59952b48c5ba225
Tom Ashworth <tom@phuu.net>
2013-06-25 13:30:29 +0100

    Refactor processors to use config object.

--- public/js/processors/processor.js
+++ public/js/processors/processor.js
@@ -1,5 +1,8 @@
-var Processor = function (url, init, handler) {
-  var failed = false;
+var createProcessor = function (opts) {
+  var failed = false,
+      url = opts.url,
+      handler = opts.handler,
+      init = opts.init;
 
   // Overwritten when the script loads
   var callback = function () {
@@ -7,11 +10,6 @@ var Processor = function (url, init, handler) {
     failed = true;
     return '';
   };
-
-  if (typeof handler === 'undefined') {
-    handler = init;
-    init = null;
-  }
 
   var script = document.createElement('script');
   script.src = url;
@@ -37,137 +35,184 @@ var Processor = function (url, init, handler) {
 
   document.body.appendChild(script);
 
-  return function () {
+  var proxyCallback = function () {
     return callback.apply(this, arguments);
   };
+
+  proxyCallback.name = opts.name;
+  proxyCallback.extensions = opts.extensions;
+
+  return proxyCallback;
 };
 
 var processors = jsbin.processors = {
   coffeescript: function (ready) {
-    return new Processor(jsbin.static + '/js/vendor/coffee-script.js', function () {
-      $.getScript(jsbin.static + '/js/vendor/codemirror3/mode/coffeescript/coffeescript.js', ready);
-    }, function (source) { 
-      var renderedCode = '';
-      try {
-        renderedCode = CoffeeScript.compile(source, {
-          bare: true
-        });
-      } catch (e) {
-        console && console.error(e.message);
+    return createProcessor({
+      name: 'coffeescript',
+      extensions: ['coffee'],
+      url: jsbin.static + '/js/vendor/coffee-script.js',
+      init: function () {
+        $.getScript(jsbin.static + '/js/vendor/codemirror3/mode/coffeescript/coffeescript.js', ready);
+      },
+      handler: function (source) {
+        var renderedCode = '';
+        try {
+          renderedCode = CoffeeScript.compile(source, {
+            bare: true
+          });
+        } catch (e) {
+          console && console.error(e.message);
+        }
+        return renderedCode;
       }
-      return renderedCode;
     });
   },
   typescript: function (ready) {
-    return new Processor(jsbin.static + '/js/vendor/typescript.min.js', ready, function (source) {
-      var noop = function () {};
-      var outfile = { 
-        source: "", 
-        Write: function (s) { 
-          this.source += s; 
-        }, 
-        WriteLine: function (s) { 
-          this.source += s + "\n"; 
-        }, 
-        Close: noop 
-      };
+    return createProcessor({
+      name: 'typescript',
+      extensions: ['ts'],
+      url: jsbin.static + '/js/vendor/typescript.min.js',
+      init: ready,
+      handler: function (source) {
+        var noop = function () {};
+        var outfile = {
+          source: "",
+          Write: function (s) {
+            this.source += s;
+          },
+          WriteLine: function (s) {
+            this.source += s + "\n";
+          },
+          Close: noop
+        };
 
-      var outerr = { 
-        Write: noop,
-        WriteLine: noop,
-        Close: noop
-      };
+        var outerr = {
+          Write: noop,
+          WriteLine: noop,
+          Close: noop
+        };
 
-      var parseErrors = [];
+        var parseErrors = [];
 
-      var compiler = new TypeScript.TypeScriptCompiler(outfile, outerr);
+        var compiler = new TypeScript.TypeScriptCompiler(outfile, outerr);
 
-      compiler.setErrorCallback(function (start, len, message) { 
-        parseErrors.push({ start: start, len: len, message: message }); 
-      });
-      compiler.parser.errorRecovery = true;
+        compiler.setErrorCallback(function (start, len, message) {
+          parseErrors.push({ start: start, len: len, message: message });
+        });
+        compiler.parser.errorRecovery = true;
 
-      compiler.addUnit(source, 'jsbin.ts');
-      compiler.typeCheck();
-      compiler.reTypeCheck();
-      compiler.emit();
+        compiler.addUnit(source, 'jsbin.ts');
+        compiler.typeCheck();
+        compiler.reTypeCheck();
+        compiler.emit();
 
-      for (var i = 0, len = parseErrors.length; i < len; i++) {
-        console.log('Error Message: ' + parseErrors[i].message);
-        console.log('Error Start: ' + parseErrors[i].start);
-        console.log('Error Length: ' + parseErrors[i].len);
+        for (var i = 0, len = parseErrors.length; i < len; i++) {
+          console.log('Error Message: ' + parseErrors[i].message);
+          console.log('Error Start: ' + parseErrors[i].start);
+          console.log('Error Length: ' + parseErrors[i].len);
+        }
+
+        return outfile.source;
       }
-
-      return outfile.source;
     });
   },
   markdown: function (ready) {
-    return new Processor(jsbin.static + '/js/vendor/markdown.js', function () {
-      $.getScript(jsbin.static + '/js/vendor/codemirror3/mode/markdown/markdown.js', ready);
-    }, function (source) {
-      return markdown.toHTML(source);
+    return createProcessor({
+      name: 'markdown',
+      extensions: ['md', 'markdown', 'mdown'],
+      url: jsbin.static + '/js/vendor/markdown.js',
+      init: function () {
+        $.getScript(jsbin.static + '/js/vendor/codemirror3/mode/markdown/markdown.js', ready);
+      },
+      handler: function (source) {
+        return markdown.toHTML(source);
+      }
     });
   },
   processing: function (ready) {
-    return new Processor(jsbin.static + '/js/vendor/processing.min.js', function () {
-      $('#library').val( $('#library').find(':contains("Processing")').val() ).trigger('change');
-      // init and expose jade
-      $.getScript(jsbin.static + '/js/vendor/codemirror3/mode/clike/clike.js', ready);
-    }, function (source) {
-      source = [
-        '(function(){',
-        '  var canvas = document.querySelector("canvas");',
-        '  if (!canvas) {',
-        '    canvas = document.createElement("canvas");',
-        '    (document.body || document.documentElement).appendChild(canvas);',
-        '  }',
-        '  canvas.width = window.innerWidth;',
-        '  canvas.height = window.innerHeight;',
-        '  var sketchProc = ' + Processing.compile(source).sourceCode + ';',
-        '  var p = new Processing(canvas, sketchProc);',
-        '})();'
-      ].join('\n');
+    return createProcessor({
+      name: 'processing',
+      extensions: ['pde'],
+      url: jsbin.static + '/js/vendor/processing.min.js',
+      init: function () {
+        $('#library').val( $('#library').find(':contains("Processing")').val() ).trigger('change');
+        // init and expose jade
+        $.getScript(jsbin.static + '/js/vendor/codemirror3/mode/clike/clike.js', ready);
+      },
+      handler: function (source) {
+        source = [
+          '(function(){',
+          '  var canvas = document.querySelector("canvas");',
+          '  if (!canvas) {',
+          '    canvas = document.createElement("canvas");',
+          '    (document.body || document.documentElement).appendChild(canvas);',
+          '  }',
+          '  canvas.width = window.innerWidth;',
+          '  canvas.height = window.innerHeight;',
+          '  var sketchProc = ' + Processing.compile(source).sourceCode + ';',
+          '  var p = new Processing(canvas, sketchProc);',
+          '})();'
+        ].join('\n');
 
-      return source;
+        return source;
+      }
     });
   },
   jade: function (ready) {
-    return new Processor(jsbin.static + '/js/vendor/jade.js', function () {
-      // init and expose jade
-      window.jade = require('jade');
-      ready();
-    }, function (source) {
-      return jade.compile(source, { pretty: true })();
+    return createProcessor({
+      name: 'jade',
+      extensions: ['jade'],
+      url: jsbin.static + '/js/vendor/jade.js',
+      init: function () {
+        // init and expose jade
+        window.jade = require('jade');
+        ready();
+      },
+      handler: function (source) {
+        return jade.compile(source, { pretty: true })();
+      }
     });
   },
   less: function (ready) {
-    return new Processor(jsbin.static + '/js/vendor/less-1.3.0.min.js', function () {
-      $.getScript(jsbin.static + '/js/vendor/codemirror3/mode/less/less.js', ready);
-    }, function (source) {
-      var css = '';
+    return createProcessor({
+      name: 'less',
+      extensions: ['less'],
+      url: jsbin.static + '/js/vendor/less-1.3.0.min.js',
+      init: function () {
+        $.getScript(jsbin.static + '/js/vendor/codemirror3/mode/less/less.js', ready);
+      },
+      handler: function (source) {
+        var css = '';
 
-      less.Parser().parse(source, function (err, result) {
-        if (err) {
-          console && console.error(err);
-          return source;
-        }
-        css = $.trim(result.toCSS());
-      });
-      return css;
+        less.Parser().parse(source, function (err, result) {
+          if (err) {
+            console && console.error(err);
+            return source;
+          }
+          css = $.trim(result.toCSS());
+        });
+        return css;
+      }
     });
   },
   stylus: function (ready) {
-    return new Processor(jsbin.static + '/js/vendor/stylus.js', ready, function (source) {
-      var css = '';
+    return createProcessor({
+      name: 'stylus',
+      extensions: ['styl'],
+      url: jsbin.static + '/js/vendor/stylus.js',
+      init: ready,
+      handler: function (source) {
+        var css = '';
 
-      stylus(source).render(function (err, result) {
-        if (err) {
-          console && console.error(err);
-          return;
-        }
-        css = $.trim(result);
-      });
-      return css;
+        stylus(source).render(function (err, result) {
+          if (err) {
+            console && console.error(err);
+            return;
+          }
+          css = $.trim(result);
+        });
+        return css;
+      }
     });
   },
   traceur: function (ready) {
@@ -176,32 +221,38 @@ var processors = jsbin.processors = {
         ProjectWriter,
         ErrorReporter,
         hasError;
-    return new Processor(jsbin.static + '/js/vendor/traceur.js', function () {
-      // Only create these once, when the processor is loaded
-      $('#library').val( $('#library').find(':contains("Traceur")').val() ).trigger('change');
-      SourceMapConsumer = traceur.outputgeneration.SourceMapConsumer;
-      SourceMapGenerator = traceur.outputgeneration.SourceMapGenerator;
-      ProjectWriter = traceur.outputgeneration.ProjectWriter;
-      ErrorReporter = traceur.util.ErrorReporter;
-      ready();
-    }, function (source) {
-      hasError = false;
+    return createProcessor({
+      name: 'traceur',
+      extensions: ['traceur'],
+      url: jsbin.static + '/js/vendor/traceur.js',
+      init: function () {
+        // Only create these once, when the processor is loaded
+        $('#library').val( $('#library').find(':contains("Traceur")').val() ).trigger('change');
+        SourceMapConsumer = traceur.outputgeneration.SourceMapConsumer;
+        SourceMapGenerator = traceur.outputgeneration.SourceMapGenerator;
+        ProjectWriter = traceur.outputgeneration.ProjectWriter;
+        ErrorReporter = traceur.util.ErrorReporter;
+        ready();
+      },
+      handler: function (source) {
+        hasError = false;
 
-      var reporter = new ErrorReporter();
-      reporter.reportMessageInternal = function(location, kind, format, args) {
-        window.console.error(ErrorReporter.format(location, format, args));
-      };
+        var reporter = new ErrorReporter();
+        reporter.reportMessageInternal = function(location, kind, format, args) {
+          window.console.error(ErrorReporter.format(location, format, args));
+        };
 
-      var url = location.href;
-      var project = new traceur.semantics.symbols.Project(url);
-      var name = 'jsbin';
+        var url = location.href;
+        var project = new traceur.semantics.symbols.Project(url);
+        var name = 'jsbin';
 
-      var sourceFile = new traceur.syntax.SourceFile(name, source);
-      project.addFile(sourceFile);
-      var res = traceur.codegeneration.Compiler.compile(reporter, project, false);
+        var sourceFile = new traceur.syntax.SourceFile(name, source);
+        project.addFile(sourceFile);
+        var res = traceur.codegeneration.Compiler.compile(reporter, project, false);
 
-      var msg = '/*\nIf you\'ve just translated to JS, make sure traceur is in the HTML panel.\nThis is terrible, sorry, but the only way we could get around race conditions. Eat me.\nHugs & kisses,\nDave xox\n*/\ntry{window.traceur = top.traceur;}catch(e){}\n';
-      return msg + ProjectWriter.write(res);
+        var msg = '/*\nIf you\'ve just translated to JS, make sure traceur is in the HTML panel.\nThis is terrible, sorry, but the only way we could get around race conditions. Eat me.\nHugs & kisses,\nDave xox\n*/\ntry{window.traceur = top.traceur;}catch(e){}\n';
+        return msg + ProjectWriter.write(res);
+      }
     });
   }
 };

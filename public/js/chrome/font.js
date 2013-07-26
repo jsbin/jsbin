b04c7bc647567212e5ef754bfd5f79853bea570b
Remy Sharp <remy@remysharp.com>
2013-07-26 23:26:47 +0100

    ability to change font size via user setting

---
+++ public/js/chrome/font.js
@@ -0,0 +1,40 @@
+/** =========================================================================
+ * font
+ * Reads user setting and configures the CodeMirror font size
+ * ========================================================================== */
+/*global jsbin:true*/
+var font = (function (document) {
+  var head = document.getElementsByTagName('head')[0],
+      selectors = '#output li, #exec, .fakeInput, #history, .fakeInput:before, #exec:before, .editbox .CodeMirror',
+      size = jsbin.settings.font || 14;
+
+  // via http://stackoverflow.com/questions/2041495/create-dynamic-inline-stylesheet
+  function font(size) {
+    var cssText = selectors + '{ font-size: ' + size + 'px; }',
+        el = document.createElement('style');
+
+    el.type= 'text/css';
+    if (el.styleSheet) {
+      el.styleSheet.cssText = cssText;//IE only
+    } else {
+      el.appendChild(document.createTextNode(cssText));
+    }
+    head.appendChild(el);
+  }
+
+  if (Object.defineProperty) {
+    Object.defineProperty(jsbin.settings, 'font', {
+      get: function () {
+        return size;
+      },
+      set: function (val) {
+        size = val * 1;
+        font(size);
+      }
+    });
+  }
+
+  font(size);
+
+  return font;
+})(document);

24d2b842c6b74a2e377cc956effcbb558528c6ad
Remy Sharp <remy@remysharp.com>
2014-01-03 15:30:45 +0000

    Fixed post-update and add RSVP to jsbin

--- build/post-update.js
+++ build/post-update.js
@@ -3,6 +3,7 @@ var fs = require('fs'),
     path = require('path'),
     version = require('../package').version,
     semver = require('semver'),
+    RSVP = require('rsvp'),
     script = process.argv[1];
 
 if (process.argv[2] && semver.valid(process.argv[2]) === process.argv[2]) {
@@ -17,11 +18,14 @@ if (process.argv[2] && semver.valid(process.argv[2]) === process.argv[2]) {
     main(v.trim());
   });
 } else {
-  console.log('Either test with ' + script + ' <version> or ' + script + ' commit to run in live mode');
+  console.log('Either test with `' + script + ' <prev-version>`\nor `' + script + ' commit` to run in live mode');
 }
 
 function main(oldVersion) {
   console.log('[jsbin post-update] Testing if there is any migration from ' + oldVersion + ' -> ' + version);
+
+  return getVersions(oldVersion, version);
+
   fs.readdir(path.join(__dirname, 'upgrade'), function (err, files) {
     files.forEach(function (file) {
       var dir = path.join(__dirname, 'upgrade', file);
@@ -29,17 +33,83 @@ function main(oldVersion) {
         if (stat.isDirectory()) {
           // test for semver
           if (semver.valid(file) === file) {
-            if (semver.satisfies(file, oldVersion + ' - ' + version)) {
+            if (semver.satisfies(file, '>=' + oldVersion + ' <' + version)) {
               console.log('[jsbin post-update] Run everything for ' + file + ' migration:');
               fs.readdir(dir, function (err, files) {
                 files.forEach(function (file) {
                   console.log('  - ' + path.join(dir, file));
                 });
               });
+            } else {
+              console.log('noop');
             }
           }
         }
       });
     });
+  });
+}
+
+function readdir(upgradeDir) {
+  return new RSVP.Promise(function (resolve, reject) {
+    fs.readdir(upgradeDir, function (err, files) {
+      files.forEach(function (file) {
+        var dir = path.join(__dirname, 'upgrade', file);
+        fs.stat(dir, function (err, stat) {
+          if (stat.isDirectory()) {
+            // test for semver
+            if (semver.valid(file) === file) {
+              resolve(file);
+            }
+          }
+        });
+      });
+    });
+  });
+}
+
+function getVersionUpdate(file) {
+  return new RSVP.Promise(function (resolve, reject) {
+    var dir = path.join(__dirname, 'upgrade', file);
+    fs.stat(dir, function (err, stat) {
+      if (stat.isDirectory()) {
+        // test for semver
+        if (semver.valid(file) === file) {
+          resolve(file);
+        } else {
+          resolve(false);
+        }
+      } else {
+        resolve(false);
+      }
+    });
+  });
+}
+
+function getVersions(oldVersion, version) {
+  var dir = path.join(__dirname, 'upgrade');
+  fs.readdir(dir, function (err, files) {
+    var promises = [];
+
+    files.forEach(function (file) {
+      promises.push(getVersionUpdate(file));
+    });
+
+    RSVP.all(promises).then(function (files) {
+      files.filter(function (file) {
+        return file !== false;
+      }).sort(function (a, b) {
+        return semver.gt(a, b) ? 1 : -1;
+      }).forEach(function (file) {
+        if (semver.satisfies(file, '>=' + oldVersion + ' <' + version)) {
+          console.log('[jsbin post-update] Run everything for ' + file + ' migration:');
+          var dir = path.join(__dirname, 'upgrade', file);
+          fs.readdirSync(dir).forEach(function (file) {
+            console.log('  - ' + path.join(dir, file));
+          });
+        }
+      });
+    });
+
   });
 }

8468b5f8eee112aad7b946bb9cd643026d9abe87
Fabien O'Carroll <fabienallouis@gmail.com>
2014-01-06 10:57:36 +0000

    Removing the escape of &

--- public/js/render/render.js
+++ public/js/render/render.js
@@ -105,11 +105,9 @@ var getPreparedCode = (function () {
     // contains '$$' it's replaced to '$' - thus breaking Prototype code. This method
     // gets around the problem.
     if (!hasHTML && hasJS) {
-      source = "<pre>\n" + js.replace(/[<>&]/g, function (m) {
+      source = "<pre>\n" + js.replace(/[<>]/g, function (m) {
         if (m == '<') return '&lt;';
         if (m == '>') return '&gt;';
-        // if (m == '"') return '&quot;';
-        if (m == '&') return '&amp;';
       }) + "</pre>";
     } else if (re.code.test(source)) {
       parts = source.split('%code%');

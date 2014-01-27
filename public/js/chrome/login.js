7f8ac92825d40cf2e4fdee16173e0b3c230792c9
Fabien O'Carroll <fabien@allou.is>
2014-01-27 19:58:49 +0000

    Adding ajax loading functionality for browsers that support History API

--- public/js/chrome/login.js
+++ public/js/chrome/login.js
@@ -1,12 +1,39 @@
-var $username = $('#username'),
-    $password = $('#password'),
-    $email = $('#email');
+/* globals $ */
+(function () {
+  'use strict';
+  // If browser doesn't support HTML5 History API
+  // We just let the anchor tag do its work
+  if (!window.history || !window.history.pushState) {
+    return;
+  }
+  
+  // Cache refrence to the content container
+  var $body = $('#body');
 
+  function attachTabClickHandlers() {
 
-// if ($('#homebtn').length) {
-//   jsbin.settings.home = document.cookie.split('home=')[1].split(';')[0];
-//   document.title = jsbin.settings.home + '@' + document.title;
-// }
-//
+    $('.tab').click(function (event) {
+      event.preventDefault();
+      var url = event.target.href;
 
-// 
+      // Don't bother loading content we already have
+      if (url === window.location.href) {
+        return;
+      }
+
+      $.ajax({
+        url: url,
+        success: function(htmlData) {
+          // Fill our content containe with new stuff
+          $body.html(htmlData);
+          // Reattach event handlers as we have partially new DOM
+          attachTabClickHandlers();
+          // Add to history
+          window.history.pushState(null, null, url);
+        }
+      });
+    });
+  }
+  // Kick it all off with initial event handlers
+  attachTabClickHandlers();
+}());

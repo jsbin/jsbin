67d33430b56c8cbc1471467320be2743fea83b25
Fabien O'Carroll <fabien@allou.is>
2014-01-28 11:39:24 +0000

    Adding history#pushstate functionality

--- public/js/chrome/login.js
+++ public/js/chrome/login.js
@@ -13,15 +13,27 @@
   // Cache initial GET request
   cachedHtml[window.location.href] = $('#body').html();
 
+  function pushState (url) {
+    console.log('state pushed');
+    window.history.pushState(null, null, url);
+  }
+
   function renderNewHtml (url, htmlData) {
-    cachedHtml[url] = htmlData;
+    var $htmlData = $(htmlData);
+    if (!cachedHtml[url]) {
+      cachedHtml[url] = $htmlData;
+    }
     // Fill our content container with new stuff
-    var content = $('<div>').html(htmlData).find('.form-container').html();
+    var content = $htmlData.siblings('.form-container').html();
     $formContainer.html(content);
     // Reattach event handlers as we have partially new DOM
     attachTabClickHandlers();
-    // Add to history
-    window.history.pushState(null, null, url);
+  }
+
+  function handlePopstateChanges() {
+    console.log(window.location.href);
+    var url = window.location.href;
+    renderNewHtml(url, cachedHtml[url]);
   }
 
   function attachTabClickHandlers() {
@@ -32,22 +44,27 @@
 
       // Don't bother loading content we already have
       if (url === window.location.href) {
+        console.log("the same");
         return;
       }
       // If we've already downloaded this html, use that
       var cachedHtmlData = cachedHtml[url];
       if (cachedHtmlData) {
-        return renderNewHtml(url, cachedHtmlData);
+        renderNewHtml(url, cachedHtmlData);
+        return pushState(url);
       }
 
       // Otherwise load it from the server
       $.ajax({
         url: url,
-        success: renderNewHtml.bind(null, url)
+        success: renderNewHtml.bind(null, url),
+        complete: pushState.bind(null, url)
       });
 
     });
   }
+
   // Kick it all off with initial event handlers
   attachTabClickHandlers();
+  window.addEventListener('popstate', handlePopstateChanges);
 }());

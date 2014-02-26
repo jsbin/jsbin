f43a7e625cb348ecf60cf27bda97cf937be869e8
Daniel Hug <danielthehug@gmail.com>
2014-02-25 20:33:01 -0800

    Clean up edit.js
    Doesn't change functionality at all.

--- public/js/render/edit.js
+++ public/js/render/edit.js
@@ -1,64 +1,43 @@
-// shows this is run through jsbin & you can edit
+// "Edit in JS Bin" button setup
 function jsbinShowEdit(options) {
-  if (window.location.hash == '#noedit') return;
-  var ie = (!+"\v1");
+  if (window.location.hash === '#noedit') return;
 
-  function hide() {
-    if (over === false) el.style.top = '-60px';
-  }
+  var moveTimer, over,
+  doc = document,
+  aEL = 'addEventListener',
+  path = window.location.pathname,
+  style = doc.createElement('link'),
+  btn = doc.createElement('a');
 
-  var el = document.createElement('a'),
-      over = false;
+  // Add button:
+  btn.id = 'edit-with-js-bin';
+  btn.href = path + (path.slice(-1) === '/' ? '' : '/') + 'edit';
+  btn.innerHTML = 'Edit in JS Bin <img src="' + options.root + '/images/favicon.png" width="16" height="16">';
+  doc.body.appendChild(btn);
 
-  el.id = 'edit-with-js-bin';
-  el.href = window.location.pathname + (window.location.pathname.substr(-1) == '/' ? '' : '/') + 'edit';
-
-  el.innerHTML = 'Edit in JS Bin <img src="' + options.root + '/images/favicon.png">';
-
-  var over;
-  el.onmouseover = function () {
-    over = true;
-    this.style.top = 0;
-  };
-
-  el.onmouseout = function () {
-    over = false;
-    hide();
-  };
-
-  var style = document.createElement('link');
+  // Style button:
   style.setAttribute('rel', 'stylesheet');
   style.setAttribute('href', options.root + '/css/edit.css');
+  doc.getElementsByTagName('head')[0].appendChild(style);
 
-  var moveTimer = null;
-  setTimeout(function () {
-    try {
-      document.getElementsByTagName('head')[0].appendChild(style);
-      document.body.appendChild(el);
-      setTimeout(hide, 2000);
 
-      if (document.addEventListener) {
-        document.addEventListener('mousemove', show, false);
-        document.addEventListener('mouseout', function () {
-          over = false;
-        }, false);
-      } else {
-        document.attachEvent('onmousemove', show);
-        document.attachEvent('onmouseout', function () {
-          over = false;
-        }, false);
-      }
-    } catch (e) {}
-  }, 100);
+  // show / hide button:
+  btn.onmouseover = btn.onmouseout = function() {
+    over = !over;
+    (over ? show : hide)();
+  };
 
   function show() {
-    // if (!ie && (el.style.top*1) == 0) { // TODO IE compat
-      el.style.top = 0;
-    // } else if (ie) {
-      // el.style.top = 1;
-      // el.style.display = 'block';
-    // }
     clearTimeout(moveTimer);
+    btn.style.top = '0';
     moveTimer = setTimeout(hide, 2000);
   }
+
+  function hide() {
+    if (!over) btn.style.top = '-60px';
+  }
+
+  show();
+  if (aEL in doc) doc[aEL]('mousemove', show, false);
+  else doc.attachEvent('mousemove', show);
 }

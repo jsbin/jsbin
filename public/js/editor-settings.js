80462b2dd408da99feff36ec60f998e12bdf1374
Remy Sharp <remy@remysharp.com>
2014-03-01 00:12:25 +0000

    Tidy up accounts. Serialise/deserialise not ok
    Still needs some work to get the syncing working. Got a feeling I might have to delete the code, re-plan and do it again (because there's a number of different ways it can happen).

--- public/js/editor-settings.js
+++ public/js/editor-settings.js
@@ -1,16 +1,19 @@
-// ;(function(){
+(function(){
+	'use strict';
+
+	/*globals $, CodeMirror */
+
 	// abstract this to a function so i can easily implement the TODO below;
 	function getCurrentSettings(){
 		// TODO do something here with a server sent object, merge it into localStorage...
 		// We do now want to lose sync;
-		return JSON.parse(localStorage.settings || '{}').editor;
+		return JSON.parse(localStorage.settings || '{}').editor || {
+
+		};
 	}
 	function pick(obj, keys) {
 		var copy = {};
-		console.log(obj);
-		console.log(keys);
 		for (var i = 0, len = keys.length; i < len; i++) {
-			console.log(obj[keys[i]]);
 			copy[keys[i]] = obj[keys[i]];
 		}
 		return copy;
@@ -22,65 +25,68 @@
 		'theme',
 		'tabSize',
 		'lineWrapping',
+		'lineNumbers',
 		'font'
-	]
+	];
 
 
 	// setup variables;
 	var $textarea = $('textarea');
 	var currentSettings = getCurrentSettings();
 
-	console.log(currentSettings);
-	var editor = CodeMirror.fromTextArea($textarea[0], $.extend({
-		mode: "javascript"
+	var editor = window.editor = CodeMirror.fromTextArea($textarea[0], $.extend({
+		mode: 'text/html'
 	}, currentSettings));
 
-	var $CodeMirror = $('.CodeMirror')
+	var $CodeMirror = $('.CodeMirror');
 
 	// setup font-size  this has a little more work behind it.
 	if (currentSettings.font) {
 		$CodeMirror.css('font-size', currentSettings.font + 'px');
 		editor.refresh();
 	}
-	var $fontsize = $('#font-size')
-	if(!$fontsize.val()) {
+	var $fontsize = $('#font-size');
+	if (!$fontsize.val()) {
 		$fontsize.val(parseInt($CodeMirror.css('font-size')));
 	}
 
 	// Setup the rest
 	var $theme = $('#theme').val(editor.getOption('theme'));
 	var $tabSize = $('#tabSize').val(editor.getOption('tabSize'));
-	var $vimMode = $('#vimMode').attr('checked', editor.getOption('vimMode'))
-	var $lineWrapping = $('#lineWrapping').attr('checked', editor.getOption('lineWrapping'))
-  var $indentWithTabs = $('#indentWithTabs').val(editor.getOption('indentWithTabs') ? 'Tabs' : 'Spaces');
-	var $_csrf = $('#_csrf');
+	var $vimMode = $('#vimMode').prop('checked', editor.getOption('vimMode'));
+	var $lineWrapping = $('#lineWrapping').prop('checked', editor.getOption('lineWrapping'));
+	var $lineNumbers = $('#lineNumbers').prop('checked', editor.getOption('lineNumbers'));
+	var tabs = editor.getOption('indentWithTabs');
+  var $indentWithTabs = $('#indentWithTabs').prop('checked', tabs);
+  $('#indentWithSpaces').prop('checked', !tabs);
+	var $csrf = $('#_csrf');
 
 	// Listeners
 	$('input[type="number"], input[type="text"]').on('input', saveSettings);
-	$('input[type="checkbox"]').on('change', saveSettings);
+	$(':checkbox, :radio').on('change', saveSettings);
 	$('select').on('change', saveSettings);
 
-	function saveSettings(event){
+	function saveSettings() {
 		// First we update the codemirror instance on page
-		editor.setOption('vimMode', !!$vimMode.attr('checked'));
-		editor.setOption('lineWrapping', !!$lineWrapping.attr('checked'));
-    editor.setOption('indentWithTabs', !!parseInt($indentWithTabs.val()));
+		editor.setOption('vimMode', $vimMode.prop('checked'));
+		editor.setOption('lineWrapping', $lineWrapping.prop('checked'));
+		editor.setOption('lineNumbers', $lineNumbers.prop('checked'));
+    editor.setOption('indentWithTabs', $indentWithTabs.prop('checked'));
 		editor.setOption('tabSize', $tabSize.val());
 		//editor.setOption('indentUnit', $tabSize.val());
 		editor.setOption('theme', $theme.val());
-		editor.setOption('font', $fontsize.val());
+		// editor.setOption('font', $fontsize.val());
 		$CodeMirror.css('font-size', $fontsize.val()+'px');
 		editor.refresh();
 
 		// Merge all our settings together
-		var localStorageSettings = JSON.parse(localStorage.settings|| '{}');
+		var localStorageSettings = JSON.parse(localStorage.settings || '{}');
 		var codemirrorSettings = pick(editor.options, settingsKeys);
-		var newSettings = $.extend(localStorageSettings.editor, codemirrorSettings)
+		var newSettings = $.extend(localStorageSettings.editor, codemirrorSettings);
 
 		// Save locally
-		console.log('codemirrorSettings');
-		console.log(codemirrorSettings);
 		localStorageSettings.editor = newSettings;
+		localStorageSettings.font = $fontsize.val();
 		localStorage.settings = JSON.stringify(localStorageSettings);
 
 		// Save on server
@@ -89,16 +95,16 @@
 			dataType: 'json',
 			data: {
 				settings: JSON.stringify(newSettings),
-				_csrf: $_csrf.val()
+				_csrf: $csrf.val()
 			},
-			success: function(data) {
+			success: function() {
 				console.log('success');
 			},
-			error: function(data) {
+			error: function() {
 				console.log('there was an error saving');
 			}
 		});
 
 	}
 
-// })();
+})();
--- public/js/jsbin.js
+++ public/js/jsbin.js
@@ -67,6 +67,7 @@ if (storedSettings === "undefined") {
 window.jsbin.settings = $.extend(JSON.parse(storedSettings || '{}'), jsbin.settings);
 if (jsbin.user) {
   $.extend(window.jsbin.settings.editor, jsbin.user.settings);
+  window.jsbin.settings.font = jsbin.user.settings.font;
 }
 // if the above code isn't dodgy, this for hellz bells is:
 jsbin.mobile = /WebKit.*Mobile.*|Android/.test(navigator.userAgent);
@@ -104,7 +105,12 @@ if (jsbin.settings.codemirror) {
 }
 
 if (jsbin.settings.editor.theme) {
-  $(document.documentElement).addClass('cm-s-' + jsbin.settings.editor.theme);
+  $(document.documentElement).addClass('cm-s-' + jsbin.settings.editor.theme.split(' ')[0]);
+  var link = document.createElement('link');
+  link.rel = 'stylesheet';
+  link.href = jsbin['static'] + '/js/vendor/codemirror3/theme/' + jsbin.settings.editor.theme.split(' ')[0] + '.css?' + jsbin.version;
+  document.getElementsByTagName('head')[0].appendChild(link);
+
 }
 
 // Add a pre-filter to all ajax requests to add a CSRF header to prevent
@@ -190,6 +196,14 @@ var $window = $(window),
 
 $window.unload(unload);
 
+window.addEventListener('storage', function (e) {
+  if (e.storageArea === localStorage && e.key === 'settings') {
+    console.log('updating from storage');
+    console.log(JSON.parse(localStorage.settings).editor);
+    jsbin.settings = JSON.parse(localStorage.settings);
+  }
+});
+
 // hack for Opera because the unload event isn't firing to capture the settings, so we put it on a timer
 if ($.browser.opera) {
   setInterval(unload, 500);
@@ -232,25 +246,6 @@ if (navigator.userAgent.indexOf(' Mac ') !== -1) (function () {
   var el = $('#keyboardHelp')[0];
   el.innerHTML = el.innerHTML.replace(/ctrl/g, 'cmd').replace(/Ctrl/g, 'ctrl');
 })();
-
-if (false) { //window.top !== window && location.pathname.indexOf('/embed') === -1) {
-  // we're framed, to switch in to embed mode
-  jsbin.embed = true;
-  jsbin.saveDisabled = true;
-  $('#saveform').attr('target', '_blank');
-  $('html').addClass('embed');
-  // remove elements that we don't need
-  $('#homemenu').closest('.menu').remove();
-  $('#login').closest('.menu').remove();
-  $('#register').closest('.menu').remove();
-  $('#library').closest('.menu').remove();
-  $('#sharemenu').remove();
-  $('a.brand').removeClass('button-dropdown').click(function (event) {
-    this.hash = '';
-    event.stopImmediatePropagation();
-    return true;
-  }).after('<a href="' + jsbin.getURL() + '/save" target="_blank" class="button save">Save</a>');
-}
 
 if (jsbin.embed) {
   $window.on('focus', function () {

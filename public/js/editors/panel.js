58ca075ba257a6bd4c20f2b7e2dfab9d6d54d646
Giulia Alfonsi <electric.g@gmail.com>
2014-03-05 11:58:11 +0000

    Removed emmet toggle comment for javascript and implemented the CodeMirror one.

--- public/js/editors/panel.js
+++ public/js/editors/panel.js
@@ -66,41 +66,41 @@ var foldFunc = {
 // this is a bit of a fudge to get multiline commenting working
 // for JavaScript. It's a fudge because emmet doesn't support
 // JavaScript as a language at all, so we inherit code our own comment style.
-var vocab = emmet.require('resources').getVocabulary('system');
-vocab.javascript = 'javascript';
-emmet.require('resources').setVocabulary(vocab, 'system');
+// var vocab = emmet.require('resources').getVocabulary('system');
+// vocab.javascript = 'javascript';
+// emmet.require('resources').setVocabulary(vocab, 'system');
 
 // totally over the top - but cleanest way to add comments to JavaScript
-var emmetToggleComment = emmet.require('actions').get('toggle_comment');
-emmet.require('actions').add('toggle_comment', function(editor) {
-  var info = emmet.require('editorUtils').outputInfo(editor);
-  if (info.syntax == 'javascript') {
-    // in case our editor is good enough and can recognize syntax from
-    // current token, we have to make sure that cursor is not inside
-    // 'style' attribute of html element
-    var editorUtils = emmet.require('editorUtils');
-    var selection = editor.getSelection();
-    var range = editor.getCurrentLineRange();
-    var line = editor.getCurrentLine();
-    var caretPos = editor.getCaretPos();
-    var tag = emmet.require('htmlMatcher').tag(info.content, caretPos);
-    if ((selection.length) || (tag && tag.open.range.inside(caretPos))) {
-      return emmetToggleComment.fn(editor);
-    } else {
-      if (line.trim().indexOf('//') == 0) {
-        editor.setCaretPos(caretPos);
-        editor.replaceContent(editorUtils.unindent(editor, line.replace(/(\s*?)\/\/\s?/, '$1')), range.start, range.end, false);
-        editor.setCaretPos(caretPos - 3);
-      } else {
-        editor.setCaretPos(caretPos);
-        editor.replaceContent(editorUtils.unindent(editor, '// ' + line), range.start, range.end, false);
-        editor.setCaretPos(caretPos + 3);
-      }
-    }
-  } else {
-    return emmetToggleComment.fn(editor);
-  }
-});
+// var emmetToggleComment = emmet.require('actions').get('toggle_comment');
+// emmet.require('actions').add('toggle_comment', function(editor) {
+//   var info = emmet.require('editorUtils').outputInfo(editor);
+//   if (info.syntax == 'javascript') {
+//     // in case our editor is good enough and can recognize syntax from
+//     // current token, we have to make sure that cursor is not inside
+//     // 'style' attribute of html element
+//     var editorUtils = emmet.require('editorUtils');
+//     var selection = editor.getSelection();
+//     var range = editor.getCurrentLineRange();
+//     var line = editor.getCurrentLine();
+//     var caretPos = editor.getCaretPos();
+//     var tag = emmet.require('htmlMatcher').tag(info.content, caretPos);
+//     if ((selection.length) || (tag && tag.open.range.inside(caretPos))) {
+//       return emmetToggleComment.fn(editor);
+//     } else {
+//       if (line.trim().indexOf('//') == 0) {
+//         editor.setCaretPos(caretPos);
+//         editor.replaceContent(editorUtils.unindent(editor, line.replace(/(\s*?)\/\/\s?/, '$1')), range.start, range.end, false);
+//         editor.setCaretPos(caretPos - 3);
+//       } else {
+//         editor.setCaretPos(caretPos);
+//         editor.replaceContent(editorUtils.unindent(editor, '// ' + line), range.start, range.end, false);
+//         editor.setCaretPos(caretPos + 3);
+//       }
+//     }
+//   } else {
+//     return emmetToggleComment.fn(editor);
+//   }
+// });
 
 // Tern
 var ternServer;
@@ -326,6 +326,9 @@ var Panel = function (name, settings) {
       });
       panel.editor.addKeyMap({
         'Cmd-D': 'deleteLine'
+      });
+      panel.editor.addKeyMap({
+        'Cmd-/': function(cm) { CodeMirror.commands.toggleComment(cm); }
       });
     }
 

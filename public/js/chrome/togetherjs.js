9d2532ffcf754232bdcc29721182f5c48bb686ab
Giulia Alfonsi <electric.g@gmail.com>
2014-03-07 14:06:06 +0000

    Added button, integration with our user profile, reload after close.

--- public/js/chrome/togetherjs.js
+++ public/js/chrome/togetherjs.js
@@ -1,17 +1,21 @@
 (function () {
-  /*jsbin:true */
-  'use strict';
-
   if (window.location.hash.indexOf('togetherjs=') !== -1) {
     jsbin.saveDisabled = true;
     jsbin.sandbox = true;
     jsbin.togetherjs = true;
-    console.log('togetherjs');
+    console.log('togetherjs init');
   }
 
   var name = jsbin.user ? jsbin.user.name : null;
   var avatar = jsbin.user ? jsbin.user.avatar : null;
 
-  var TogetherJSConfig_getUserName = function () { return name; };
-  var TogetherJSConfig_getUserAvatar = function () { return avatar; };
+  TogetherJSConfig_getUserName = function () { return name; };
+  TogetherJSConfig_getUserAvatar = function () { return avatar; };
+  // TogetherJSConfig_on_ready = function() {
+  //   console.log('togetherjs ready');
+  // };
+  TogetherJSConfig_on_close = function () {
+    console.log('togetherjs close');
+    window.location.reload();
+  };
 })();

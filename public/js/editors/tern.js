30b9b0b85e732399fa67cccf615ef268e1abe38c
Remy Sharp <remy@remysharp.com>
2014-03-20 13:32:48 +0000

    use strict on new "module"

--- public/js/editors/tern.js
+++ public/js/editors/tern.js
@@ -1,118 +1,124 @@
-// jsbin.settings.addons.tern = true
-var ternServer;
-var ternLoaded = {};
+(function () {
+  'use strict';
 
-var initTern = function(editor, defs){
-  var keyMap = {
-    'Ctrl-I': function(cm) { ternServer.showType(cm); },
-    'Ctrl-Space': function(cm) { ternServer.complete(cm); }
-  };
-  if (typeof defs === 'undefined') {
-    defs = [];
-  }
-  ternServer = new CodeMirror.TernServer({
-    defs: defs,
-    useWorker: false,
-    cm: editor
-  });
-  editor.addKeyMap(keyMap);
-  editor.on('cursorActivity', function(cm) { ternServer.updateArgHints(cm); });
-};
+  /*globals $, jsbin, CodeMirror, simpleJsHint, template */
 
-var addTernDefinition = function(def) {
-  if (typeof ternServer === 'object') {
-    ternServer.options.defs.push(def);
+  // jsbin.settings.addons.tern = true
+  var ternServer;
+  var ternLoaded = {};
+
+  var initTern = function(editor, defs){
+    var keyMap = {
+      'Ctrl-I': function(cm) { ternServer.showType(cm); },
+      'Ctrl-Space': function(cm) { ternServer.complete(cm); }
+    };
+    if (typeof defs === 'undefined') {
+      defs = [];
+    }
     ternServer = new CodeMirror.TernServer({
-      defs: ternServer.options.defs,
-      useWorker: ternServer.options.useWorker,
-      tooltipType: ternServer.options.tooltipType,
-      cm: ternServer.options.cm
+      defs: defs,
+      useWorker: false,
+      cm: editor
     });
-  }
-};
+    editor.addKeyMap(keyMap);
+    editor.on('cursorActivity', function(cm) { ternServer.updateArgHints(cm); });
+  };
 
-// Load the json defition of the library
-var loadTernDefinition = function(name, file) {
-  if (!ternLoaded[name]) {
-    $.ajax({
-      url: file,
-      dataType: 'json',
-      success: function(data) {
-        addTernDefinition(data);
-        ternLoaded[name] = true;
-      }
-    });
-  }
-};
+  var addTernDefinition = function(def) {
+    if (typeof ternServer === 'object') {
+      ternServer.options.defs.push(def);
+      ternServer = new CodeMirror.TernServer({
+        defs: ternServer.options.defs,
+        useWorker: ternServer.options.useWorker,
+        tooltipType: ternServer.options.tooltipType,
+        cm: ternServer.options.cm
+      });
+    }
+  };
 
-// Load the actual js library
-var loadTernFile = function(name, file) {
-  if (!ternLoaded[name]) {
-    $.ajax({
-      url: file,
-      dataType: 'script',
-      success: function(data) {
-        ternServer.server.addFile(name, data);
-        ternLoaded[name] = true;
-      }
-    });
-  }
-};
-
-var loadTern = function(editor) {
-  initTern(editor, ternBasicDefs);
-  ternLoaded.ecma5 = true;
-  ternLoaded.browser = true;
-};
-
-var searchTernDefinition = function(htmlCode) {
-  if (jsbin.settings.addons.tern === true) {
-    for (var i = 0; i < ternDefinitions.length; i++) {
-      if (ternDefinitions[i].match.test(htmlCode)) {
-        if (ternDefinitions[i].type === 'def') {
-          loadTernDefinition(ternDefinitions[i].name, ternDefinitions[i].file);
+  // Load the json defition of the library
+  var loadTernDefinition = function(name, file) {
+    if (!ternLoaded[name]) {
+      $.ajax({
+        url: file,
+        dataType: 'json',
+        success: function(data) {
+          addTernDefinition(data);
+          ternLoaded[name] = true;
         }
-        else {
-          loadTernFile(ternDefinitions[i].name, ternDefinitions[i].file);
+      });
+    }
+  };
+
+  // Load the actual js library
+  var loadTernFile = function(name, file) {
+    if (!ternLoaded[name]) {
+      $.ajax({
+        url: file,
+        dataType: 'script',
+        success: function(data) {
+          ternServer.server.addFile(name, data);
+          ternLoaded[name] = true;
+        }
+      });
+    }
+  };
+
+  var loadTern = function(editor) {
+    initTern(editor, ternBasicDefs);
+    ternLoaded.ecma5 = true;
+    ternLoaded.browser = true;
+  };
+
+  var searchTernDefinition = function(htmlCode) {
+    if (jsbin.settings.addons.tern === true) {
+      for (var i = 0; i < ternDefinitions.length; i++) {
+        if (ternDefinitions[i].match.test(htmlCode)) {
+          if (ternDefinitions[i].type === 'def') {
+            loadTernDefinition(ternDefinitions[i].name, ternDefinitions[i].file);
+          }
+          else {
+            loadTernFile(ternDefinitions[i].name, ternDefinitions[i].file);
+          }
         }
       }
     }
-  }
-};
+  };
 
-// Overwrite the autocomplete function to use tern
-if (jsbin.settings.addons.tern === true) {
-  CodeMirror.commands.autocomplete = function(cm) {
-    if (CodeMirror.snippets(cm) === CodeMirror.Pass) {
-      var pos = cm.getCursor();
-      var tok = cm.getTokenAt(pos);
-      var indent = '';
-      if (cm.options.indentWithTabs) {
-        indent = '\t';
-      }
-      else {
-        indent = new Array(cm.options.indentUnit + 1).join(' ');
-      }
-      if (tok.string === ';') {
+  // Overwrite the autocomplete function to use tern
+  if (jsbin.settings.addons.tern === true) {
+    CodeMirror.commands.autocomplete = function(cm) {
+      if (CodeMirror.snippets(cm) === CodeMirror.Pass) {
+        var pos = cm.getCursor();
+        var tok = cm.getTokenAt(pos);
+        var indent = '';
+        if (cm.options.indentWithTabs) {
+          indent = '\t';
+        } else {
+          indent = new Array(cm.options.indentUnit + 1).join(' ');
+        }
+
+        if (tok.string === ';') {
+          return cm.replaceRange(indent, pos);
+        }
+        if (tok.string.trim() !== '') {
+          return ternServer.complete(cm);
+        }
         return cm.replaceRange(indent, pos);
       }
-      if (tok.string.trim() !== '') {
-        return ternServer.complete(cm);
-      }
-      return cm.replaceRange(indent, pos);
-    }
-  };
-}
-// Use the simple autocompletion
-else {
-  CodeMirror.commands.autocomplete = simpleJsHint;
-}
+    };
+  } else {
+    // Use the simple autocompletion
+    CodeMirror.commands.autocomplete = simpleJsHint;
+  }
 
-if (jsbin.settings.addons.tern === true) {
-  loadTern(editors.javascript.editor);
-  searchTernDefinition(template.html);
+  if (jsbin.settings.addons.tern === true) {
+    loadTern(jsbin.panels.panels.javascript.editor);
+    searchTernDefinition(template.html);
 
-  $('#library').bind('change', function () {
-    searchTernDefinition(editors.html.getCode());
-  });
-}
+    $('#library').bind('change', function () {
+      searchTernDefinition(jsbin.panels.panels.html.getCode());
+    });
+  }
+
+})();

6060cbc3237274ffea9160433a2d891d37c30f0b
Giulia Alfonsi <electric.g@gmail.com>
2014-04-01 11:06:55 +0100

    #1350: Removed togetherjs

--- public/js/chrome/save.js
+++ public/js/chrome/save.js
@@ -37,13 +37,9 @@ $('a.save').click(function (event) {
 
   analytics.milestone();
   // if save is disabled, hitting save will trigger a reload
-  // if it's connecting to a togetherjs session, save will not trigger a reload
   var ajax = true;
   if (jsbin.saveDisabled === true) {
     ajax = false;
-  }
-  if (jsbin.togetherjs === true) {
-    ajax = true;
   }
   saveCode('save', ajax);
 

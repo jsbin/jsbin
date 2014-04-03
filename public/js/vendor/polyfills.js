a6640d91b756e85e8f6b71c08489ec146dbeff34
Giulia Alfonsi <electric.g@gmail.com>
2014-04-03 13:37:33 +0100

    Changed array reduce polyfill

--- public/js/vendor/polyfills.js
+++ public/js/vendor/polyfills.js
@@ -34,50 +34,39 @@ if (!Array.prototype.indexOf) {
 // ES5 15.4.4.21
 // http://es5.github.com/#x15.4.4.21
 // https://developer.mozilla.org/en/Core_JavaScript_1.5_Reference/Objects/Array/reduce
-if (!Array.prototype.reduce) {
-    Array.prototype.reduce = function reduce(fun /*, initial*/) {
-        var object = toObject(this),
-            self = splitString && _toString(this) == "[object String]" ?
-                this.split("") :
-                object,
-            length = self.length >>> 0;
-
-        // If no callback function or if callback is not a callable function
-        if (_toString(fun) != "[object Function]") {
-            throw new TypeError(fun + " is not a function");
+if ('function' !== typeof Array.prototype.reduce) {
+  Array.prototype.reduce = function(callback/*, opt_initialValue*/){
+    'use strict';
+    if (null === this || 'undefined' === typeof this) {
+      throw new TypeError(
+         'Array.prototype.reduce called on null or undefined');
+    }
+    if ('function' !== typeof callback) {
+      throw new TypeError(callback + ' is not a function');
+    }
+    var index, value, t = Object( this ),
+        length = t.length >>> 0,
+        isValueSet = false;
+    if (1 < arguments.length) {
+      value = arguments[1];
+      isValueSet = true;
+    }
+    for (index = 0; length > index; ++index) {
+      if (index in t) {
+        if (isValueSet) {
+          value = callback(value, t[index], index, t);
         }
-
-        // no value to return if no initial value and an empty array
-        if (!length && arguments.length == 1) {
-            throw new TypeError("reduce of empty array with no initial value");
+        else {
+          value = t[index];
+          isValueSet = true;
         }
-
-        var i = 0;
-        var result;
-        if (arguments.length >= 2) {
-            result = arguments[1];
-        } else {
-            do {
-                if (i in self) {
-                    result = self[i++];
-                    break;
-                }
-
-                // if array contains no values, no initial value to return
-                if (++i >= length) {
-                    throw new TypeError("reduce of empty array with no initial value");
-                }
-            } while (true);
-        }
-
-        for (; i < length; i++) {
-            if (i in self) {
-                result = fun.call(void 0, result, self[i], i, object);
-            }
-        }
-
-        return result;
-    };
+      }
+    }
+    if (!isValueSet) {
+      throw new TypeError('Reduce of empty array with no initial value');
+    }
+    return value;
+  };
 }
 
 

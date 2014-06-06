6055bc4109f4ccc582095f912bf68fb796e6d63e
Giulia Alfonsi <electric.g@gmail.com>
2014-06-06 11:19:42 +0100

    Save the state in localStorage

--- public/js/chrome/toppanel.js
+++ public/js/chrome/toppanel.js
@@ -1,5 +1,5 @@
 (function () {
-  /*global jsbin, $, $body*/
+  /*global jsbin, $, $body, $document*/
   'use strict';
 
   if (jsbin.settings.gui === undefined) {
@@ -7,14 +7,19 @@
   }
   if (jsbin.settings.gui.toppanel === undefined) {
     jsbin.settings.gui.toppanel = true;
+    localStorage.setItem('settings', JSON.stringify(jsbin.settings));
   }
 
   var removeToppanel = function() {
+    jsbin.settings.gui.toppanel = false;
+    localStorage.setItem('settings', JSON.stringify(jsbin.settings));
     $body.addClass('toppanel-close');
     $body.removeClass('toppanel');
   };
 
   var showToppanel = function() {
+    jsbin.settings.gui.toppanel = true;
+    localStorage.setItem('settings', JSON.stringify(jsbin.settings));
     $body.removeClass('toppanel-close');
     $body.addClass('toppanel');
   };
@@ -30,19 +35,16 @@
   $('.toppanel-hide').click(function(event) {
     event.preventDefault();
     goSlow(event);
-    jsbin.settings.gui.toppanel = false;
     removeToppanel();
   });
   $('.toppanel-logo').click(function(event) {
     event.preventDefault();
     goSlow(event);
-    jsbin.settings.gui.toppanel = true;
     showToppanel();
   });
   $document.keydown(function (event) {
     if (event.which == 27) {
       if ($body.hasClass('toppanel')) {
-        jsbin.settings.gui.toppanel = false;
         removeToppanel();
       }
     }

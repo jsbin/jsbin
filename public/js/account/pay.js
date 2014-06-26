4264fcb49bd10988470d49aa8c13c85c90fbd588
Remy Sharp <remy@remysharp.com>
2014-06-26 22:16:05 +0100

    Client side VAT & payment taking working

--- public/js/account/pay.js
+++ public/js/account/pay.js
@@ -1,11 +1,59 @@
-$(function () {
-  /*global $*/
+jQuery(function ($) {
+  /*global Stripe*/
   'use strict';
-  $('.form-container').card({
-    container: $('.card-wrapper')
+
+  if (![].map) {
+    Array.prototype.map = function (fn) {
+      return $.map(this, fn);
+    };
+  }
+
+  /**
+   * Make the form friendly for credit card input
+   */
+  var cardnumber = $('#cardnumber').payment('formatCardNumber').on('blur', function () {
+    $(this).closest('span').toggleClass('valid', $.payment.validateCardNumber(this.value));
   });
 
-  var eu = ['AT', 'BE', 'BG', 'CY', 'CZ', 'DE', 'DK', 'EE', 'EL', 'ES', 'FI', 'FR', 'GB', 'HR', 'HU', 'IE', 'IT', 'LT', 'LU', 'LV', 'MT', 'NL', 'PL', 'PT', 'RO', 'SE', 'SI', 'SK'];
+  // Expiry is a single field, so we need to split it up to validate
+  var expiry = $('#expiry').payment('formatCardExpiry').on('blur', function () {
+    var data = this.value.split('/').map(function (s) {
+      return s.trim();
+    });
+
+    $(this).closest('span').toggleClass('valid', $.payment.validateCardExpiry(data[0], data[1]));
+  });
+
+  // CVC
+  var cvc = $('#cvc').payment('formatCardCVC').on('blur', function () {
+    var card = $.payment.cardType(cardnumber.val());
+    $(this).closest('span').toggleClass('valid', $.payment.validateCardCVC(this.value, card));
+  });
+
+  var email = $('#email').on('blur', function () {
+    $(this).closest('span').toggleClass('valid', this.validity && this.validity.valid);
+  });
+
+  email.closest('span').toggleClass('valid', this.validity && this.validity.valid);
+
+
+  $('input[name=buyer_type]').on('change', function () {
+    if (this.value === 'business' && this.checked) {
+      $('.business').fadeTo(50, 1).find(':input').prop('disabled', false);
+    } else {
+      $('.business').fadeTo(50, 0.5).find(':input').prop('disabled', true);
+    }
+  }).trigger('change');
+
+
+  /**
+   * Automatically select the country based on the visitor's IP, and if they change
+   * the country and the country they select is in the EU, update the VAT prefix
+   */
+
+  var eu = ['AT', 'BE', 'BG', 'CY', 'CZ', 'DE', 'DK', 'EE', 'EL', 'ES', 'FI',
+            'FR', 'GB', 'HR', 'HU', 'IE', 'IT', 'LT', 'LU', 'LV', 'MT', 'NL',
+            'PL', 'PT', 'RO', 'SE', 'SI', 'SK'];
 
   var countryEl = $('#country');
   var vatEl = $('#vat');
@@ -28,20 +76,96 @@ $(function () {
     }
   });
 
+
+  /**
+   * Validate the VAT registration number (which we'll also do on the server
+   * side) using a simple heroku app.
+   */
   $('#validateVat').on('click', function (event) {
     event.preventDefault();
-    var vat = countryEl.val() + vatEl[0].value.replace(/\D/g, '');
+    var vatNum = vatEl[0].value.replace(/\s/g, '');
+    var vat = countryEl.val().toLowerCase() + vatNum;
 
-    $.getJSON('//vat-validator.herokuapp.com/' + vat + '?callback=?', function (data) {
-      if (data) {
-        if (data.valid) {
-          vatEl[0].className = 'valid';
-        } else {
-          vatEl[0].className = 'invalid';
+    if (vatNum) {
+      vatEl.addClass('validating');
+      $.getJSON('//vat-validator.herokuapp.com/' + vat + '?callback=?', function (data) {
+        if (data.error) {
+          console.error('500 error on VAT lookup');
+          return setTimeout(function () {
+            $('#validateVat').click();
+          }, 2000);
         }
-      } else {
-        console.log('cannot validate VAT');
-      }
-    });
+
+        if (data) {
+          if (data.valid) {
+            vatEl[0].className = 'valid';
+          } else {
+            vatEl[0].className = 'invalid';
+          }
+        } else {
+          console.log('cannot validate VAT');
+          vatEl[0].className = '';
+        }
+        vatEl.removeClass('validating');
+      });
+    } else {
+      vatEl[0].className = '';
+    }
   });
+
+
+  /**
+   * Do the Stripe dance.
+   */
+
+  $('#payment-form').submit(function () {
+    var $form = $(this);
+
+    // Disable the submit button to prevent repeated clicks
+    $form.find('button#pay').prop('disabled', true);
+
+    var expiryData = expiry.val().split('/').map(function (s) {
+      return s.trim();
+    });
+
+    Stripe.card.createToken({
+      number: $('#cardnumber').val(),
+      cvc: $('#cvc').val(),
+      'exp_month': expiryData[0],
+      'exp_year': expiryData[1]
+    }, stripeResponseHandler);
+
+
+    // Prevent the form from submitting with the default action
+    return false;
+  });
+
+  function stripeResponseHandler(status, response) {
+    var $form = $('#payment-form');
+
+    if (response.error) {
+      // Show the errors on the form
+      $form.find('.payment-errors').html('<b class="icon-info-circle"></b> ' + response.error.message);
+      $form.find('button').prop('disabled', false);
+    } else {
+      if (response.card.country) {
+        if (countryEl.val() !== response.card.country) {
+
+        }
+      }
+
+      $form.find('button').prop('disabled', false);
+      console.log(response);
+
+      // response contains id and card, which contains additional card details
+      var token = response.id;
+      // Insert the token into the form so it gets submitted to the server
+      $form.append($('<input type="hidden" name="stripeToken" />').val(token));
+      // and submit
+      // $form.get(0).submit();
+    }
+  }
+
+
+
 });

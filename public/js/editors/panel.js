a84813332f53f2fa236558a1d28378e16fd18e2b
Giulia Alfonsi <electric.g@gmail.com>
2014-07-18 16:04:17 +0100

    Added jade syntax highlight to codemirror

--- public/js/editors/panel.js
+++ public/js/editors/panel.js
@@ -14,7 +14,8 @@ var editorModes = {
   less: 'text/x-less',
   sass: 'text/x-sass',
   scss: 'text/x-scss',
-  processing: 'text/x-csrc'
+  processing: 'text/x-csrc',
+  jade: 'text/x-jade'
 };
 
 var badChars = new RegExp('[\u200B\u0080-\u00a0]', 'g');

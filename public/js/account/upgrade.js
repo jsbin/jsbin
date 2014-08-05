fc0844a13220c5edffcdea9b928a208dc3c933a6
Giulia Alfonsi <electric.g@gmail.com>
2014-08-05 14:24:43 +0100

    First version of the design

--- public/js/account/upgrade.js
+++ public/js/account/upgrade.js
@@ -10,4 +10,25 @@
     $form.attr('action', originalAction + '?' + $input.val());
   });
 
+  // Show/hide email login/registration block
+  $formLogin = $('#form-login');
+  $('#btn-login').on('click', function(event) {
+    event.preventDefault();
+    $formLogin.toggle();
+  });
+
+  // Show login/registration
+  $formLoginTabs = $formLogin.find('.tabs');
+  $formLoginTab = $formLogin.find('a.tab');
+  $formLoginTab.on('click', function(event) {
+    event.preventDefault();
+    var $this = $(this);
+    if ($this.hasClass('login')) {
+      $formLogin.addClass('login');
+      $formLogin.removeClass('register');
+    } else {
+      $formLogin.addClass('register');
+      $formLogin.removeClass('login');
+    }
+  })
 }());

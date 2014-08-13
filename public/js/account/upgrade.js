9fc88506249c27a32226e480fab101fcfa22a26d
Remy Sharp <remy@remysharp.com>
2014-08-13 17:17:27 +0100

    Register + sign up (without errors) works

--- public/js/account/upgrade.js
+++ public/js/account/upgrade.js
@@ -1,17 +1,17 @@
-/* globals $ */
 (function () {
+  /* globals $ */
+  'use strict';
 
   var $input = $('#coupon_code');
   var $form = $('#stripe_pro_month');
   var originalAction = $form.attr('action');
 
   $input.on('change', function () {
-    'use strict';
     $form.attr('action', originalAction + '?' + $input.val());
   });
 
   // Show/hide email login/registration block
-  $formLogin = $('#form-login');
+  var $formLogin = $('#form-login');
   $('#btn-login').on('click', function(event) {
     event.preventDefault();
     $formLogin.toggle();
@@ -19,29 +19,25 @@
   });
 
   // Show login/registration
-  $formLoginTabs = $formLogin.find('.tabs');
-  $formLoginTab = $formLogin.find('a.tab');
-  $formLoginLogin = $formLogin.find('.upgrade-fieldset.login');
-  $formLoginRegister = $formLogin.find('.upgrade-fieldset.register');
+  var $formLoginTab = $formLogin.find('a.tab');
+  var $formLoginLogin = $formLogin.find('.upgrade-fieldset.login');
+  var $formLoginRegister = $formLogin.find('.upgrade-fieldset.register');
   $formLoginTab.on('click', function(event) {
     event.preventDefault();
-    var $this = $(this);
-    if ($this.hasClass('login')) {
-      $formLogin.addClass('login');
-      $formLogin.removeClass('register');
+    if ($(this).hasClass('login')) {
+      $formLogin.addClass('login').removeClass('register');
       $formLoginRegister.attr('disabled', 'disabled');
       $formLoginLogin.attr('disabled', false);
     } else {
-      $formLogin.addClass('register');
-      $formLogin.removeClass('login');
+      $formLogin.addClass('register').removeClass('login');
       $formLoginRegister.attr('disabled', false);
       $formLoginLogin.attr('disabled', 'disabled');
     }
-  })
+  });
 
   // Steps
-  openClass = 'open';
-  $upgradeSummaries = $('.upgrade-summary');
+  var openClass = 'open';
+  var $upgradeSummaries = $('.upgrade-summary');
 
   $upgradeSummaries.on('click', function() {
     $(this).closest('.upgrade-details').toggleClass(openClass);
@@ -51,7 +47,11 @@
 
   $('.upgrade-details-next').on('click', function(event) {
     event.preventDefault();
+    if ($('#signup-email').val().length) {
+      // remove the second email field as we have their email already
+      $('#email').closest('div').remove();
+    }
     $(this).closest('.upgrade-details').removeClass(openClass)
       .nextAll('.upgrade-details').addClass(openClass);
-  })
+  });
 }());

c4525b14868d407eb319bfa5c044c2608df0c3c8
Fabien O'Carroll <fabien@allou.is>
2014-08-19 11:00:32 +0100

    Prefix IIFE's with ;
    This fixes issue #1822
    
    The problem was that the Date.now polyfill returns a function and that function was being called with our IIFE as the argument.

--- public/js/vendor/polyfills.js
+++ public/js/vendor/polyfills.js
@@ -148,7 +148,7 @@ Date.now||(Date.now=function(){return+new Date})
 
 // MIT license
 
-(function() {
+;(function() {
     var lastTime = 0;
     var vendors = ['ms', 'moz', 'webkit', 'o'];
     for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
@@ -173,7 +173,7 @@ Date.now||(Date.now=function(){return+new Date})
         };
 }());
 
-(function () {
+;(function () {
   function Empty() {}
   var slice = [].slice;
 

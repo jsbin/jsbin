af7fad15076fabec378971da13bea3d764f40357
Giulia Alfonsi <electric.g@gmail.com>
2014-08-20 17:03:56 +0100

    Moved admin pages into a single one

--- public/js/admin/admin.js
+++ public/js/admin/admin.js
@@ -8,7 +8,7 @@
 
     var form = $(this);
     var url = form.find('input[name=bin]').val();
-    var $csrf = $('#_csrf');
+    var $csrf = form.find('input[name=_csrf]');
     var $responseFeedback = form.find('.responseFeedback');
     var reg = /(?:https*:\/\/jsbin.com\/)*([\w]+)\/*(\d+)*/i;
     // var reg = new RegExp();
@@ -33,6 +33,34 @@
         var data = $.parseJSON(jqXHR.responseText) || {};
         if (jqXHR.status === 200) {
           $responseFeedback.show().text('Bin flagged succesfully');
+        }
+      }
+    });
+  });
+
+
+  $('#flag-user').submit(function (event) {
+    event.preventDefault();
+
+    var form = $(this);
+    var username = form.find('input[name=username]').val();
+    var $csrf = form.find('input[name=_csrf]');
+    var $responseFeedback = form.find('.responseFeedback');
+
+    $responseFeedback.show().text('Checking...');
+
+    $.ajax({
+      url: form.attr('action'),
+      data: {
+        username: username,
+        _csrf: $csrf.val()
+      },
+      type: 'POST',
+      dataType: 'json',
+      complete: function (jqXHR) {
+        var data = $.parseJSON(jqXHR.responseText) || {};
+        if (jqXHR.status === 200) {
+          $responseFeedback.show().text('User flagged succesfully');
         }
       }
     });

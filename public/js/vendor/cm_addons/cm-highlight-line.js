7b28aebd03780e6dd51adcad15b513f8e6f29eda
Fabien O'Carroll <fabien@allou.is>
2014-10-03 12:29:52 +0100

    typo: highlighLine -> highlightLine

--- public/js/vendor/cm_addons/cm-highlight-line.js
+++ public/js/vendor/cm_addons/cm-highlight-line.js
@@ -23,7 +23,7 @@
   var WRAP_CLASS = 'CodeMirror-highlight-line';
   var BACK_CLASS = 'CodeMirror-highlight-line-background';
 
-  CodeMirror.defineOption('highlighLine', false, function(cm, val, old) {
+  CodeMirror.defineOption('highlightLine', false, function(cm, val, old) {
     var prev = old && old !== CodeMirror.Init;
     if (val && !prev) {
       cm.state.highlightedLines = [];

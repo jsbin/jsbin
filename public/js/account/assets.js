14556f3db18ba11e23268bf8378cfa924217e6d7
Remy Sharp <remy@remysharp.com>
2014-10-09 11:59:28 +0100

    Tweaking asset management style
    Goes slightly against @dannyhope's design, but it's works on the page.

--- public/js/account/assets.js
+++ public/js/account/assets.js
@@ -19,30 +19,35 @@
     return size.toFixed(1) + ' ' + units[i];
   }
 
-  $('#files').on('click', 'button', function () {
+  $('#files').on('click', 'button', function (e) {
+    e.preventDefault();
+    e.stopPropagation();
     var $button = $(this).prop('disabled', true);
     $.post('/account/assets/remove', {
       _csrf: csrf,
-      key: $button.closest('li').data('key')
+      key: $button.closest('a').data('key')
     }, function () {
-      var $li = $button.closest('li');
+      var $li = $button.closest('a');
       var size = $li.data('size') * 1;
       totalSize -= size;
-      $('#total-size').html(readableFileSize(totalSize));
+      updateFreeSpace(totalSize);
       $li.fadeTo(200, 0, function () {
         $li.remove();
       });
     });
   });
-
-  $.getJSON('/account/assets/size', function loadassets(data) {
-    var percent = (data.size / (1024 * 1024 * 1024)) * 100;
+  
+  function updateFreeSpace(size) {
+    var percent = (size / (1024 * 1024 * 1024)) * 100;
     if ((percent | 0 ) !== percent) {
       percent = percent.toFixed(1);
     }
-    $('#total-size').html(data.human + ' used ' + percent + '% of your current 1 GB limit');
-    totalSize = data.size;
+    $('#total-size').html(readableFileSize(size) + ' used ' + percent + '% of your current 1 GB limit');
+    totalSize = size;
+  }
 
+  $.getJSON('/account/assets/size', function loadassets(data) {
+    updateFreeSpace(data.size);
     var html = '';
     console.log(data.objects);
     data.objects.forEach(function (object) {
@@ -51,7 +56,10 @@
 
 
       html += '<a href="' + url + '" data-size="' + object.size + '" data-key="' + key + '" class="asset" target="_blank">';
-      html += '<span class="flex"></span><img src="' + url + '">';
+      html += '<span class="flex"></span>';
+      html += '<button><img src="' + jsbin.static + '/images/close-x.png"></button>';
+//       html += '<img src="' + url + '">';
+      html += '<div style="background-image: url(' + url.replace(/\s/g, '%20') + '), url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAJUlEQVQYV2P89evXfwY0wMbGxoguxjgUFKI7GsTH5m4M3w1ChQAneyesWb250wAAAABJRU5ErkJggg==)"></div>';
       html += '<p class="name">' + key + '</p>';
       html += '<p class="file-size">' + object.human + '</p>';
       html += '</a>';

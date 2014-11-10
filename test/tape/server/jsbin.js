76d46dc9819d46174c315c9717a14d09c8619a6d
Fabien O'Carroll <fabien@allou.is>
2014-11-10 19:19:23 +0000

    initial server side tests
    these require jsbin be runnign on port 3000

---
+++ test/tape/server/jsbin.js
@@ -0,0 +1,32 @@
+var test = require('tape');
+
+var request = require('request');
+
+test('jsbin', function (jsbin) {
+
+  jsbin.test('can connect', function (t) {
+    t.plan(1);    
+    request.get('http://localhost:3000', function (err, res, body) {
+      if (err) {
+        return t.fail(err); 
+      }
+      t.pass();
+    });
+  });
+
+  jsbin.test('can load full output', function (t) {
+    t.plan(1); 
+    request.get('http://localhost:3000/sof', function (err, res, body) {
+      if (err || body.indexOf('THIS IS A TEST') === -1) {
+        return t.fail(err || 'Did not contain expected output'); 
+      }
+      t.pass();
+    });
+  });
+
+  jsbin.end();
+
+});
+
+
+

12e25d66e51b5397cfacd7ddc148a46974846a34
Fabien O'Carroll <fabien@allou.is>
2014-11-11 12:10:26 +0000

    syntax and proper pass messages

--- test/tape/server/jsbin.js
+++ test/tape/server/jsbin.js
@@ -5,28 +5,25 @@ var request = require('request');
 test('jsbin', function (jsbin) {
 
   jsbin.test('can connect', function (t) {
-    t.plan(1);    
+    t.plan(1);
     request.get('http://localhost:3000', function (err, res, body) {
       if (err) {
         return t.fail(err); 
       }
-      t.pass();
+      t.pass('No error from jsbin homepage');
     });
   });
 
   jsbin.test('can load full output', function (t) {
     t.plan(1); 
-    request.get('http://localhost:3000/sof', function (err, res, body) {
+    request.get('http://localhost:3000/cuqequdenu', function (err, res, body) {
       if (err || body.indexOf('THIS IS A TEST') === -1) {
         return t.fail(err || 'Did not contain expected output'); 
       }
-      t.pass();
+      t.pass('Full output contained the string "THIS IS A TEST"');
     });
   });
 
   jsbin.end();
 
 });
-
-
-

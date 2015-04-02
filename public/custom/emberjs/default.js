5a91eb64b1ace373359a3ccb3ff633d80ed93392
Seth <seth.wright.08@gmail.com>
2015-04-02 14:11:06 -0400

    Update default.js

--- public/custom/emberjs/default.js
+++ public/custom/emberjs/default.js
@@ -6,6 +6,31 @@ App.Router.map(function() {
 
 App.IndexRoute = Ember.Route.extend({
   model: function() {
-    return ['red', 'yellow', 'blue'];
+    return this.store.find('color');
+  }  
+});
+
+App.ColorModel = DS.Model.extend({
+  name: DS.attr('string')
+});
+
+$.mockjax({
+  url: "/colors",
+  type: "GET",
+  status: 200,
+  statusText: "OK",
+  responseText: {
+    colors: [{
+      id: 1,
+      name: "Red"
+    },
+    {
+      id: 2,
+      name: "Green"
+    },
+    {
+      id: 3,
+      name: "Blue"
+    }]
   }
 });

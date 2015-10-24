51b02d75bb6ced03bd13b7b2f257df47f8b0f612
Prabir Shrestha <mail@prabir.me>
2015-10-23 19:45:39 -0700

    add version numbers to rx libraries

--- public/js/editors/libraries.js
+++ public/js/editors/libraries.js
@@ -649,17 +649,17 @@ var libraries = [
   },
   {
     'url': 'https://cdnjs.cloudflare.com/ajax/libs/rxjs/2.3.22/rx.all.js',
-    'label': 'rx.all',
+    'label': 'rx.all 2.3.22',
     'group': 'RxJS'
   },
   {
     'url': 'https://cdnjs.cloudflare.com/ajax/libs/rxjs/2.3.22/rx.testing.js',
-    'label': 'rx.testing',
+    'label': 'rx.testing 2.3.22',
     'group': 'RxJS'
   },
   {
     'url': 'https://cdnjs.cloudflare.com/ajax/libs/rxjs-dom/2.0.7/rx.dom.js',
-    'label': 'rx.dom',
+    'label': 'rx.dom 2.0.7',
     'group': 'RxJS'
   },
   {

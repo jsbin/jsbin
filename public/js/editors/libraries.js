c3165d704c0cad79b414c7843e77b4b3ecc976f0
Ben Lesh <blesh@netflix.com>
2015-11-09 08:53:01 -0800

    feat(RxJS): remove RxJS 2.X links
    RxJS 2 is outdated, and we probably do not want people using that

--- public/js/editors/libraries.js
+++ public/js/editors/libraries.js
@@ -663,21 +663,6 @@ var libraries = [
     'group': 'RxJS'
   },
   {
-    'url': 'https://cdnjs.cloudflare.com/ajax/libs/rxjs/2.3.22/rx.all.js',
-    'label': 'rx.all 2.3.22',
-    'group': 'RxJS'
-  },
-  {
-    'url': 'https://cdnjs.cloudflare.com/ajax/libs/rxjs/2.3.22/rx.testing.js',
-    'label': 'rx.testing 2.3.22',
-    'group': 'RxJS'
-  },
-  {
-    'url': 'https://cdnjs.cloudflare.com/ajax/libs/rxjs-dom/2.0.7/rx.dom.js',
-    'label': 'rx.dom 2.0.7',
-    'group': 'RxJS'
-  },
-  {
     'url': 'http://cdn.popcornjs.org/code/dist/popcorn.min.js',
     'label': 'Popcorn.js 1.5.6 (Core)',
     'group': 'Popcorn.js'

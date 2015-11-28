9ffc2b21639288f3195c109b266d8d67b187235b
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2015-11-28 16:08:37 +0100

    :arrow_up: Update Bootstrap to 3.3.6
    This is a trivial commit, just updating CDN links.
    The 3.3.6 resources are already available at bootstrapcdn.com
    Thanks!

--- public/js/editors/libraries.js
+++ public/js/editors/libraries.js
@@ -133,8 +133,8 @@ var libraries = [
   {
     'url': [
       'https://code.jquery.com/jquery.min.js',
-      'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css',
-      'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js'
+      'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css',
+      'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js'
     ],
     'label': 'Bootstrap Latest',
     'group': 'Bootstrap'

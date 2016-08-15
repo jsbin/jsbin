b1c8df5aa32a02abde61b43ae69257fcf553d5d3
Kirill Cherkashin <kirill@kcherkashin.org>
2016-08-14 20:38:09 -0400

    Run selenium automatically when running e2e tests

--- nightwatch.js
+++ nightwatch.js
@@ -1,5 +1,19 @@
+var glob = require('glob');
+var path = require('path');
+
+var seleniumJar = glob.sync(path.join(
+  'node_modules', 'selenium-standalone', '.selenium', 'selenium-server', '*.jar'))[0];
+
+if (!seleniumJar) {
+  throw new Error('Can\'t find selenium jar, try running "npm run selenium:install"');
+}
+
 module.exports = {
-  "src_folders" : ["test/e2e"],
+  "src_folders": ["test/e2e"],
+  "selenium": {
+    "start_process": true,
+    "server_path": seleniumJar
+  },
   "test_settings": {
     "default": {
       "launch_url": "http://127.0.0.1:3000/"

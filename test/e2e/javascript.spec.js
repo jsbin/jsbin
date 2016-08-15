e61b6cc196c4cd55c23a55b775bcac620e53019f
Kirill Cherkashin <kirill@kcherkashin.org>
2016-08-14 20:38:38 -0400

    Rewrite the test to avoid focus issues, on the standalone version of selenium

--- test/e2e/javascript.spec.js
+++ test/e2e/javascript.spec.js
@@ -2,13 +2,12 @@ module.exports = {
   'Basic JavaScript test' : function (client) {
     client
       .url(client.launch_url)
-      .waitForElementVisible('body', 1000)
       .waitForElementVisible('#panels a:nth-child(3)', 1000)
       .click('#panels a:nth-child(3)')
       .waitForElementVisible('.javascript .CodeMirror', 1000)
-      .click('.javascript .CodeMirror textarea')
-      .keys('document.body.innerHTML = \'Hello\';')
-      .waitForElementVisible('iframe', 1000)
+      .execute(function (text){
+        return $('.javascript .CodeMirror')[0].CodeMirror.setValue(text);
+      }, ["document.body.innerHTML = 'Hello'"])
       .frame(2)
       .assert.containsText('body', 'Hello')
       .end();

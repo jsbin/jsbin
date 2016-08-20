bd502f5b118b3b614628f3a90bcc73f7e022712d
Kirill Cherkashin <kirill@kcherkashin.org>
2016-08-20 09:00:37 -0400

    Add a prefix for tunnel-identifier

--- nightwatch.js
+++ nightwatch.js
@@ -28,7 +28,7 @@ module.exports = {
         platform: "OS X 10.11",
         version: "latest",
         build: 'build-' + TRAVIS_JOB_NUMBER,
-        'tunnel-identifier': TRAVIS_JOB_NUMBER,
+        'tunnel-identifier': 'id-' + TRAVIS_JOB_NUMBER,
         "username": process.env.SAUCE_USERNAME,
         "access_key": process.env.SAUCE_ACCESS_KEY
       },

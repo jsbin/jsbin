1494496c842073a860289e7a7aead8eb110538eb
voicetime <iprovod@gmail.com>
2016-09-20 09:23:25 +0300

    Add Basic top panel test in file toppanel.spec.js

---
+++ test/e2e/specs/toppanel.spec.js
@@ -0,0 +1,15 @@
+module.exports = {
+ 'Basic top panel test': function (client){
+ var data = client.globals;
+ client
+  .url(client.launch_url)
+  .waitForElementVisible('.toppanel-hide')
+   .click('.toppanel-hide')
+   .pause(data.defaultTime)
+   .waitForElementNotVisible('.toppanel-hide')
+   .click('.toppanel-logo')
+   .waitForElementVisible('.toppanel-hide')
+   .assert.urlMatch(/\/\?html,output$/)//real server adress ?html,css,output$/ . You need to find out why an address ?html,output
+  .end();
+ }
+};

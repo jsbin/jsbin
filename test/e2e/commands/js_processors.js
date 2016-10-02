59b09004d0cfadf81e2052627ee4cd92227567c3
voicetime <iprovod@gmail.com>
2016-10-02 22:56:15 +0300

    Add custom commands func for test js procesors

---
+++ test/e2e/commands/js_processors.js
@@ -0,0 +1,20 @@
+exports.command = function(selector, callback) {
+    var data = this.globals;
+    this
+      .url(this.launch_url)
+      .waitForElementVisible('#panel-javascript')
+      .click('#panel-javascript')
+      .waitForElementVisible('.javascript .CodeMirror')
+      this.click('a[href="#javascriptprocessors"]')
+      .click('a[id="processor-javascript"]').pause(data.defaultTime)
+        this.execute(function (text){
+        return $('.javascript .CodeMirror')[0].CodeMirror.setValue(text);
+      }, ["document.body.innerHTML = 'Hello'"])
+      .frame(2)
+      .assert.containsText('body', 'Hello')
+      .assert.urlMatch(/\/\w+\/edit\?html,js,output$/)
+      .end();
+
+
+  return this; // allows the command to be chained.
+};

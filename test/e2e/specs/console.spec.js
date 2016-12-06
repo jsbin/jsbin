e7a111f3520e4914b5cfe2a783d25eb47445bca9
voicetime <iprovod@gmail.com>
2016-12-06 09:11:32 +0300

    Add Test Console: output error, output value of the variable

--- test/e2e/specs/console.spec.js
+++ test/e2e/specs/console.spec.js
@@ -1,13 +1,32 @@
 module.exports = {
-    'Test Console': function (client) {
+    'Test Console: add to the body HTML text': function (client) {
         client
             .url(client.launch_url)
             .selectTab('#panel-console')
             .waitForElementVisible('div #exec')
-            .setValue('div #exec', ["document.body.innerHTML = 'Hello'",client.Keys.ENTER])
+            .setValue('div #exec', ["document.body.innerHTML = 'Hello'", client.Keys.ENTER])
             .selectOutputFrame()
             .assert.containsText('body', 'Hello')
-            .assert.urlMatch(/\/\w+\/edit\?html,js,output$/)
+            .assert.urlMatch(/\/\?html,console,output$/)
+            .end();
+    },
+    'Test Console: output error': function (client) {
+        client
+            .url(client.launch_url)
+            .selectTab('#panel-console')
+            .waitForElementVisible('div #exec')
+            .setValue('div #exec', ["b", client.Keys.ENTER])
+            .assert.containsText('.echo', 'b')
+            .assert.containsText('.str', '"b is not defined"')
+            .end();
+    },
+    'Test Console: output value of the variable ': function (client) {
+        client
+            .url(client.launch_url)
+            .selectTab('#panel-console')
+            .waitForElementVisible('div #exec')
+            .setValue('div #exec', ["a = 1", client.Keys.ENTER])
+            .assert.containsText('.lit', '1')
             .end();
     }
 };

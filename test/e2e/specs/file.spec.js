4324d82e6657179a4908112884f15f77d24392ee
voicetime <iprovod@gmail.com>
2016-12-21 08:46:35 +0300

    Add Archive and Unarchive  bin test in file.spec.js

--- test/e2e/specs/file.spec.js
+++ test/e2e/specs/file.spec.js
@@ -13,12 +13,12 @@ module.exports = {
             .assert.urlMatch(/\/\?html,js,output/)
             .end();
     },
-    'Archive  bin test': function (client) {
+    'Archive and Unarchive  bin test': function (client) {
         client
             .url(client.launch_url)
             .click('#loginbtn').pause(client.globals.defaultTimeout)
-            .setValue('#login-username', '')
-            .setValue('#login-key', '')
+            .setValue('#login-username', 'voicetime@yandex.ru')
+            .setValue('#login-key', '12345678')
             .pause(client.globals.defaultTimeout);
         client
             .click('input[value="Log in"]')
@@ -29,9 +29,10 @@ module.exports = {
             .pause(client.globals.defaultTimeout)
             .assert.urlMatch(/\/\w+\/edit\?html,js,output$/)
             .click('a.brand.button.button-dropdown.group.button-dropdown-arrow')
-            .click('a.archivebin.button.group').pause(client.globals.defaultTimeout)
+            .click('a.archivebin.button.group')
             .pause(client.globals.defaultTimeout)
-            .click('a.toppanel-button.homebtn').pause(client.globals.defaultTimeout)
+            .click('a.toppanel-button.homebtn')
+            .pause(client.globals.defaultTimeout)
             .click('#toggleArchive')
             .assert.urlMatch(/\/\w+\/edit\?/).pause(client.globals.defaultTimeout);
         client.url(function (response) {
@@ -45,11 +46,22 @@ module.exports = {
                 .pause(client.globals.defaultTimeout)
         });
         client
-            .assert.urlMatch(/\/\w+\/1\/edit\?html,js,output$/);
-
+            .assert.urlMatch(/\/\w+\/1\/edit\?html,js,output$/)
+            .click('a.toppanel-button.homebtn')
+            .pause(client.globals.defaultTimeout)
+            .click('#toggleArchive')
+            .url(function (response) {
+                var url = response.value;
+                var arrDataUrl = url.split('/');
+                client
+                    .pause(client.globals.defaultTimeout)
+                    .click('tr[data-url="/' + arrDataUrl[3] + '/1"]')
+                    .pause(1000)
+                    .click('.unarchive').pause(1000);
+                client.expect.element('tr[data-url="/' + arrDataUrl[3] + '/1"]').to.be.not.visible;
+            });
         client.end();
     },
-
     'Add your bin description test': function (client) {
         client
             .url(client.launch_url)
@@ -79,7 +91,7 @@ module.exports = {
                     var urlClone = response.value;
                     var arrDataUrlClone = urlClone.split('/');
                     if (arrDataUrl[3] === arrDataUrlClone[3]) {
-                        console.log('WARN: Two url are the same ' + arrDataUrl[3])
+                        console.log('WARN: Two url are the same ' + arrDataUrl[3]);
                         console.log(url);
                         console.log(arrDataUrl[3]);
                         console.log(urlClone);

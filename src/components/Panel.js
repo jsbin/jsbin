6297fc59e90dfbe50f03976ca83f1963996da180
Remy Sharp <remy@remysharp.com>
2017-07-20 12:48:44 +0100

    chore: Initial create-react-app
    Starting jsbin@5 from scratch 😱

---
+++ src/components/Panel.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import CodeMirror from 'react-codemirror';
+require('codemirror/lib/codemirror.css');
+
+export default class Panel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { code: '// this is some code' };
+    this.updateCode = this.updateCode.bind(this);
+  }
+
+  updateCode(newCode) {
+    this.setState({
+      code: newCode
+    });
+  }
+
+  render() {
+    const { code } = this.state;
+    const options = { lineNumbers: true };
+
+    return (
+      <CodeMirror value={code} onChange={this.updateCode} options={options} />
+    );
+  }
+}

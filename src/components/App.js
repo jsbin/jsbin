7e13f0d18ccab75e109e9020a8710c4e14871705
Remy Sharp <remy@remysharp.com>
2017-07-20 17:16:15 +0100

    chore: move App to components

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import Splitter from 'react-split-pane';
-import Panel from './components/Panel';
-import Output from './components/Output';
-import Nav from './components/Nav';
+import Panel from './Panel';
+import Output from './Output';
+import Nav from './Nav';
 import PropTypes from 'prop-types';
 
-import './css/App.css';
+import '../css/App.css';
 
 export default class App extends Component {
   render() {
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
 import store from './store';
-import App from './App';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
 import './index.css';

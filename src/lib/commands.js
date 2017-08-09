af09359f3a5113962ece5e7dcf6306ab9d2edeb1
Remy Sharp <remy@remysharp.com>
2017-08-09 21:32:10 +0100

    chore: enable tests

--- src/lib/commands.js
+++ src/lib/commands.js
@@ -177,14 +177,14 @@ export const changeLanguage = {
 export const exportToCodePen = {
   title: 'Export to CodePen',
   run: async (dispatch, { bin, user }) => {
-    if (!user.pro) {
-      return {
-        title: 'JS Bin license required, upgrade?',
-        run: dispatch => {
-          dispatch(push('/upgrade'));
-        },
-      };
-    }
+    // if (!user.pro) {
+    //   return {
+    //     title: 'JS Bin license required, upgrade?',
+    //     run: dispatch => {
+    //       dispatch(push('/upgrade'));
+    //     },
+    //   };
+    // }
 
     const exporter = await import(/* webpackChunkName: "exporter" */ '../lib/exporter');
 

739e99c16bde9d110731bae2cce5b2d3314d5b0a
sidlogin <siddhartha.gupta@gmail.com>
2021-07-28 16:25:39 -0500

    Create basic-js-01.js

---
+++ basic-js-01.js
@@ -0,0 +1,25 @@
+console.clear();
+/* 'Let & Const' */
+
+let myFirstName = 'Sid';
+myFirstName = 'Siddharth';
+const myLastName = 'Gpt';
+
+// myLastName = 'Gupta'
+console.log('Let/Const ===>', myFirstName, myLastName);
+
+
+/* 'Arrow Function' */
+function subtraction(number) {
+  return number - 1;
+}
+
+const multiply = (number) => {
+  return number * 2;
+}
+
+const addition = (number) => number + 2;
+
+console.log('Arrow Function ===>', multiply(10), addition(10))
+
+
